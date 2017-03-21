b52c53d36cf8d9ec47d16dc771b38457a00ec178
Earvin <earvin.agamata@cid-systems.com>
2017-03-21 20:39:16 +0800

    Sequence Control

--- apanel/system/seqcontrol.php
+++ apanel/system/seqcontrol.php
@@ -2,7 +2,23 @@
 class seqcontrol extends db {
 
 	public function getValue($code, $companycode) {
-		
+		$properties = $this->setTable('wc_sequence_control')
+						->setFields('current, prefix')
+						->setWhere("companycode = '$companycode' AND code = '$code'")
+						->setLimit(1)
+						->getProperties();
+		$result = $this->setProperties($properties)
+						->runSelect()
+						->getRow();
+		$return = false;
+		if ($result) {
+			$return = $result->prefix . $result->current;
+			$result->current += 1;
+			$this->setProperties($properties)
+				->setValues((array) $result);
+			$this->runUpdate();
+		}
+		return $return;
 	}
 
 }
