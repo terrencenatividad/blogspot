311620cf8132a8e8ad2181c3212eb421860fb0cb
Earvin <earvin.agamata@cid-systems.com>
2017-04-17 14:02:07 +0800

    Update Footer

--- apanel/view/backend_footer.php
+++ apanel/view/backend_footer.php
@@ -20,9 +20,9 @@
 			</div>
 			<footer class="main-footer">
 				<div class="pull-right hidden-xs">
-					<b>Version</b> 2.3.8
+					<b>Version</b> 2.0.1
 				</div>
-				<strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+				<strong>Copyright &copy; 2017 <a href="http://cid-systems.com">Cid Systems</a>.</strong> All rights reserved.
 			</footer>
 			<aside class="control-sidebar control-sidebar-dark">
 				<ul class="nav nav-tabs nav-justified control-sidebar-tabs">
