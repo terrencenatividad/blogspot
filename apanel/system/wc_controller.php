b3775c2099a8a4efb397f3f0a5e0ea7d5c3690c1
Earvin <earvin.agamata@cid-systems.com>
2017-05-08 13:54:17 +0800

    Add Check Out Model from Different Module

--- apanel/system/wc_controller.php
+++ apanel/system/wc_controller.php
@@ -5,5 +5,20 @@ class wc_controller {
 		$this->view = new wc_view();
 	}
 
+	public function checkOutModel($model) {
+		$temp = explode('/', $model);
+		if (isset($temp[1])) {
+			$path = "modules/{$temp[0]}/model/{$temp[1]}.php";
+			if (is_file($path)) {
+				require_once($path);
+				return new $temp[1]();
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
 
 }
