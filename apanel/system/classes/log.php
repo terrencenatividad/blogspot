2b233e35759c57f2aca2a02bf3125332c5269c3a
Earvin <earvin.agamata@cid-systems.com>
2017-05-11 14:05:29 +0800

    Add Logs

---
+++ apanel/system/classes/log.php
@@ -0,0 +1,27 @@
+<?php
+class log extends db {
+
+	public function __construct() {
+		parent::__construct();
+		$ipaddress = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? end(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])) : (isset($_SERVER['REMOTE_ADDR'])? $_SERVER['REMOTE_ADDR'] : isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : '-');
+		$browser = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '-';
+		$this->data = array(
+			'companycode' => COMPANYCODE,
+			'username' => USERNAME,
+			'timestamps' => date('Y-m-d H:i:s'),
+			'activitydone' => '',
+			'ip_address' => $ipaddress,
+			'browser' => $browser,
+			'module' => MODULE_NAME,
+			'task' => MODULE_TASK
+		);
+	}
+
+	public function saveActivity($activity) {
+		$this->data['activity'] = $activity;
+		return $this->setTable('wc_admin_logs')
+				->setValue($this->data)
+				->runInsert();
+	}
+
+}
