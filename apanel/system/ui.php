79cf308ac40d2bda5e40d13a7856ea0307b199ff
Earvin <earvin.agamata@cid-systems.com>
2017-05-15 09:47:41 +0800

    Update UI Class

--- apanel/system/ui.php
+++ apanel/system/ui.php
@@ -376,7 +376,7 @@ class ui {
 
 	private function drawStaticInput() {
 		$value = ($this->add_hidden !== true && $this->add_hidden !== false && ! $this->draw ) ? $this->add_hidden : $this->value;
-		$id = ($this->add_hidden && ! $this->draw ) ? ' id="' . $this->attribute['id'] . '_static"' : '';
+		$id = ($this->add_hidden && $this->draw ) ? ' id="' . $this->attribute['id'] . '_static"' : '';
 		return '<p class="form-control-static">' . $value . '</p>';
 	}
 
