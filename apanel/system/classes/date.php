1cb5abeba7bb374ddd396456974335f0c0d8ca2a
Earvin <earvin.agamata@cid-systems.com>
2017-05-22 13:36:52 +0800

    Core: class for setting date format for site

---
+++ apanel/system/classes/date.php
@@ -0,0 +1,32 @@
+<?php
+class date {
+
+	public function dateDbFormat($date = '') {
+		$date = $this->convertDate($date, 'Y-m-d');
+		return $date;
+	}
+
+	public function datetimeDbFormat($date = '') {
+		$date = $this->convertDate($date, 'Y-m-d H:i:s');
+		return $date;
+	}
+
+	public function dateFormat($date = '') {
+		$date = $this->convertDate($date, 'M j, Y');
+		return $date;
+	}
+
+	public function datetimeFormat($date = '') {
+		$date = $this->convertDate($date, 'M j, Y h:i:s A');
+		return $date;
+	}
+
+	private function convertDate($date, $format) {
+		if (empty($date)) {
+			return date($format);
+		} else {
+			return date($format, strtotime($date));
+		}
+	}
+
+}
