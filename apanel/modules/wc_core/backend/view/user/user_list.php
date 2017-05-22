6d4c96ac68812c76ac0e8cc640872099e06438b9
Earvin <earvin.agamata@cid-systems.com>
2017-05-22 10:34:59 +0800

    Update User List

--- apanel/modules/wc_core/backend/view/user/user_list.php
+++ apanel/modules/wc_core/backend/view/user/user_list.php
@@ -7,7 +7,7 @@
 							<div class="col-md-4">
 								<div class="form-group">
 									<a href="<?= MODULE_URL ?>create" class="btn btn-primary">Create User</a>
-									<button type="button" id="item_multiple_delete" class="btn btn-danger delete_button">Delete</button>
+									<button type="button" id="item_multiple_delete" class="btn btn-danger delete_button">Delete<span></span></button>
 								</div>
 							</div>
 						</div>
