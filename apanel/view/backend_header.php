6332cdc52600fca7c19da51f91199e1f564dd2b7
Earvin <earvin.agamata@cid-systems.com>
2017-05-24 13:24:45 +0800

    Core: Add OojeemaPrime Logo

--- apanel/view/backend_header.php
+++ apanel/view/backend_header.php
@@ -65,7 +65,9 @@
 		<nav class="navbar navbar-static-top">
 			<div class="container-fluid">
 				<div class="navbar-header">
-					<a href="" class="navbar-brand"><b>Oojeema</b><i>Prime</i></a>
+					<a href="<?php echo BASE_URL ?>" class="navbar-brand navbar-logo">
+						<img src="<?php echo BASE_URL ?>assets/images/oojeema_PRIME_white_small.png" height="100%">
+					</a>
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
 						<i class="fa fa-bars"></i>
 					</button>
