2b4467757c0551b3c29e3c6f433e824346740490
Earvin <earvin.agamata@cid-systems.com>
2017-06-21 17:05:58 +0800

    Core: Update Indexes

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 if ( ! ob_start('ob_gzhandler')) {
 	ob_start();
 }
+define('PRE_PATH', '');
 define('PAGE_TYPE', 'frontend');
-require_once 'apanel/wc_site.php';
+require_once PRE_PATH . 'settings.php';
+require_once PRE_PATH . CORE_COMPONENTS . 'wc_site.php';
 
 // OB END
 while (ob_get_level() > 1 && ob_end_flush());
