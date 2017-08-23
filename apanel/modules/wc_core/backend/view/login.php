28417c625000a7bf12f5b4b79e044e818fed1020
cid_earvin <earvin.agamata@cid-systems.com>
2017-08-23 14:51:36 +0800

    Core: Add Local Copy of FontAwesome and Ionicons

--- apanel/modules/wc_core/backend/view/login.php
+++ apanel/modules/wc_core/backend/view/login.php
@@ -6,8 +6,8 @@
 		<title>Login</title>
 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 		<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+		<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/ionicons.min.css">
 		<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/AdminLTE.min.css">
 		<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/skin.css">
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
--- apanel/view/backend_header.php
+++ apanel/view/backend_header.php
@@ -6,8 +6,8 @@
 	<title><?php echo $page_title ?></title>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 	<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+	<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/ionicons.min.css">
 	<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/select2.min.css">
 	<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/daterangepicker.css">
 	<link rel="stylesheet" href="<?= BASE_URL ?>assets/css/datepicker3.css">
