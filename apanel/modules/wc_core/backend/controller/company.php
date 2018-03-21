281ecbefc239eed9f52175fdf350def83262122c
earvin <earvin.agamata@cid-systems.com>
2018-03-21 16:03:39 +0800

    Update Company Logo Upload

--- apanel/modules/wc_core/backend/controller/company.php
+++ apanel/modules/wc_core/backend/controller/company.php
@@ -164,7 +164,7 @@ class controller extends wc_controller
 			$BigImageMaxSize 		= 200; //Image Maximum height or width
 			$ThumbPrefix			= "thumb_"; //Normal thumb Prefix
 			//$DestinationDirectory	= SITE_ROOT.'/uploads/company_logo/'; //specify upload directory ends with / (slash)
-			$DestinationDirectory	= $_SERVER["DOCUMENT_ROOT"]."/webcore_modular/wc_core_components/assets/images/";
+			$DestinationDirectory	= str_replace('apanel/index.php', '', $_SERVER["SCRIPT_FILENAME"])."uploads/company_logo/";
 			$Quality 				= 90; //jpeg quality
 			##########################################
 		
