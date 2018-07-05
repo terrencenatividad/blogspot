9a9307704813c90adafae0ece9f65056a95c9ad4
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-07-05 10:58:46 +0800

    Purchase Return: Updated Restrictions for Closed Periods

--- apanel/modules/purchase_module/backend/controller/purchase_return.php
+++ apanel/modules/purchase_module/backend/controller/purchase_return.php
@@ -7,6 +7,7 @@ class controller extends wc_controller {
 		$this->input			= new input();
 		$this->logs				= new log();
 		$this->purchase_model	= new purchase_return_model();
+		$this->restrict 		= new purchase_restriction_model();
 		// $this->financial_model  = $this->checkOutModel('financials_module/financial_model');
 		$this->inventory_model	= $this->checkoutModel('inventory_module/inventory_model');
 		$this->session			= new session();
@@ -77,6 +78,9 @@ class controller extends wc_controller {
 		$data['ajax_task']			= 'ajax_create';
 		$data['ajax_post']			= '';
 		$data['show_input']			= true;
+		// Closed Date
+		$close_date 				= $this->restrict->getClosedDate();
+		$data['close_date']			= $close_date;
 		$this->view->load('purchase_return/purchase_return', $data);
 	}
 
@@ -94,6 +98,9 @@ class controller extends wc_controller {
 		$data['ajax_task']			= 'ajax_edit';
 		$data['ajax_post']			= "&voucherno_ref=$voucherno";
 		$data['show_input']			= true;
+		// Closed Date
+		$close_date 				= $this->restrict->getClosedDate();
+		$data['close_date']			= $close_date;
 		$this->view->load('purchase_return/purchase_return', $data);
 	}
 
@@ -101,7 +108,8 @@ class controller extends wc_controller {
 		$this->view->title			= 'View Purchase Return';
 		$this->fields[]				= 'stat';
 		$data						= (array) $this->purchase_model->getPurchaseReturnById($this->fields, $voucherno);
-		$data['transactiondate']	= $this->date->dateFormat($data['transactiondate']);
+		$transactiondate 			= $data['transactiondate'];
+		$data['transactiondate']	= $this->date->dateFormat($transactiondate);
 		$data['ajax_task']			= '';
 		$data['ui']					= $this->ui;
 		$data['vendor_list']		= $this->purchase_model->getVendorList();
@@ -110,6 +118,11 @@ class controller extends wc_controller {
 		$data['header_values']		= json_encode($this->purchase_model->getPurchaseReturnById($this->fields_header, $voucherno));
 		$data['voucher_details']	= json_encode($this->purchase_model->getPurchaseReturnDetails($this->fields2, $voucherno));
 		$data['show_input']			= false;
+		// Closed Date
+		$close_date 				= $this->restrict->getClosedDate();
+		$data['close_date']			= $close_date;
+		$restrict_ret 				= $this->restrict->setButtonRestriction($transactiondate);
+		$data['restrict_ret'] 	 	= $restrict_ret;
 		$this->view->load('purchase_return/purchase_return', $data);
 	}
 
@@ -183,13 +196,17 @@ class controller extends wc_controller {
 			$table = '<tr><td colspan="9" class="text-center"><b>No Records Found</b></td></tr>';
 		}
 		foreach ($pagination->result as $key => $row) {
+			$transactiondate 	=	$row->transactiondate;
+
+			$restrict_ret 		=	$this->restrict->setButtonRestriction($transactiondate);
+
 			$table .= '<tr>';
 			$dropdown = $this->ui->loadElement('check_task')
 									->addView()
-									->addEdit($row->stat == 'Returned')
-									->addDelete($row->stat == 'Returned')
+									->addEdit($row->stat == 'Returned' && !$restrict_ret)
+									->addDelete($row->stat == 'Returned' && !$restrict_ret)
 									->addPrint($row->stat == 'Returned')
-									->addCheckbox($row->stat == 'Returned')
+									->addCheckbox($row->stat == 'Returned' && !$restrict_ret)
 									->setLabels(array('delete' => 'Cancel'))
 									->setValue($row->voucherno)
 									->draw();
