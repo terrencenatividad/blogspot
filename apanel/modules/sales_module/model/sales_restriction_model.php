8e1b1363c634b1103cf345bf7a4077e4c9730a3e
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-07-05 11:11:35 +0800

    Sales Restriction Model

---
+++ apanel/modules/sales_module/model/sales_restriction_model.php
@@ -0,0 +1,40 @@
+<?php
+
+    class sales_restriction_model extends wc_model  {
+        // public function __construct() {
+        //     parent::__construct();
+        //     $this->db = new log();
+        // }
+
+        public function setButtonRestriction($transactiondate) {
+            $closed_date     =   $this->getClosedDate();
+
+            if( $closed_date > $transactiondate ){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        public function getClosedDate() {
+            $result     =   $this->db->setTable("journalvoucher")
+                                     ->setFields("transactiondate")
+                                     ->setWhere("stat = 'posted' AND source = 'closing'")
+                                     ->setOrderBy("transactiondate DESC")
+                                     ->setLimit(1)
+                                     ->runSelect()
+                                     ->getRow();
+
+            $resultArr     =   ($result)    ?   explode(" ",$result->transactiondate)   :   "";
+
+            $returnvalue    =   "";
+            if($resultArr){
+                $returnvalue    =   $resultArr[0];
+            } else {
+                $returnvalue    =   "";
+            }
+
+            return $returnvalue;
+        }
+    }
+
