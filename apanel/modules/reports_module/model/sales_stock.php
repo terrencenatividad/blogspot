5b7fc38a54bc4fbb5e4a42b20d61a98f192397c6
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-08-03 15:43:53 +0800

    Sales Per stock : Fix on format & computation

--- apanel/modules/reports_module/model/sales_stock.php
+++ apanel/modules/reports_module/model/sales_stock.php
@@ -7,95 +7,54 @@ class sales_stock extends wc_model {
 						->setWhere("stat = 'active'")
 						->runSelect()
 						->getResult();
-				//var_dump($this->db->getQuery());
 		return $result;
 	}
 
-	public function stockList($data)
-	{
-		$daterangefilter = (isset($data['daterangefilter'])) ? htmlentities($data['daterangefilter']) : ""; 
-		$warehouse       = (isset($data['warehouse']) && !empty($data['warehouse'])) ? htmlentities($data['warehouse']) : "";
-		$items 		 	 = (isset($data['items'])  && !empty($data['items'])) ? htmlentities($data['items']) : "";
-		$sort       	= (isset($data['sort']) && !empty($data['sort'])) ? htmlentities($data['sort']) : "";
-		
-		$datefilterArr		= explode(' - ',$daterangefilter);
-		$datefilterFrom		= (!empty($datefilterArr[0])) ? date("Y-m-d",strtotime($datefilterArr[0])) : "";
-		$datefilterTo		= (!empty($datefilterArr[1])) ? date("Y-m-d",strtotime($datefilterArr[1])) : "";
-		
-		$addCondition = "";
-		$addCondition .= (!empty($warehouse) && $warehouse != 'none' )? " AND invdtl.warehouse = '$warehouse' ":"";
-		
-		$addCondition .= (!empty($daterangefilter) && !is_null($datefilterArr)) ? "AND inv.transactiondate BETWEEN '$datefilterFrom' AND '$datefilterTo' " : "";
-
-		$result = $this->db->setTable('salesinvoice inv')
-						->setFields("ic.label,invdtl.itemcode,invdtl.detailparticular,FORMAT(SUM(invdtl.issueqty),0) as issueqty ,w.description warehouse,w.description,SUM(invdtl.unitprice) as unitprice ,SUM(invdtl.amount) as amount")
-						->leftJoin('salesinvoice_details invdtl ON inv.voucherno= invdtl.voucherno')
-						->innerJoin("items i ON invdtl.itemcode = i.itemcode AND invdtl.companycode = i.companycode")
-						->innerJoin("itemclass ic ON i.classid = ic.id AND i.companycode = ic.companycode")
-						->leftJoin("warehouse w ON invdtl.warehouse = w.warehousecode AND invdtl.companycode = w.companycode")
-						->setWhere("inv.stat = 'posted' $addCondition ")
-						->setOrderBy($sort)
-						->setLimit($items)
-						->setGroupBy('invdtl.itemcode')
-						->runPagination();
-
-		return $result;
-		
-	}
-
-	public function fileExport($start, $end, $warehouse, $limit, $sort)
-	{
-		$addCondition = '';
-
-		if ($start && $end){
-			$addCondition .= "AND inv.transactiondate BETWEEN '$start' AND '$end' " ;
-		}
-
-		if ($warehouse && $warehouse != 'none'){
-			$addCondition .= "AND invdtl.warehouse = '$warehouse' ";
-		}
-
-		$result = $this->db->setTable('salesinvoice inv')
-							->setFields("ic.label,invdtl.itemcode,invdtl.detailparticular,SUM(invdtl.issueqty) as issueqty ,w.description warehouse,w.description,SUM(invdtl.unitprice) as unitprice ,SUM(invdtl.amount) as amount")
-							->leftJoin('salesinvoice_details invdtl ON inv.voucherno= invdtl.voucherno')
-							->innerJoin("items i ON invdtl.itemcode = i.itemcode AND invdtl.companycode = i.companycode")
-							->innerJoin("itemclass ic ON i.classid = ic.id AND i.companycode = ic.companycode")
-							->leftJoin("warehouse w ON invdtl.warehouse = w.warehousecode AND invdtl.companycode = w.companycode")
-							->setWhere("inv.stat = 'posted' $addCondition ")
-							->setOrderBy($sort)
-							->setGroupBy('invdtl.itemcode')
-							->runSelect()
-							->getResult();
-		return $result;
-	}
-
-	public function retrieveData($table, $fields = array(), $cond = "", $join = "", $orderby = "", $groupby = "")
-		{
-			$result = $this->db->setTable($table)
-						->setFields($fields)
-						->leftJoin($join)
-						->setGroupBy($groupby)
-						->setWhere($cond)
-						->setOrderBy($orderby)
+	public function getSalesPersonList() {
+		$result = $this->db->setTable('partners')
+						->setFields("partnercode ind, CONCAT(first_name,' ',last_name) val")
+						->setWhere("stat = 'active' AND partnertype = 'sales'")
 						->runSelect()
 						->getResult();
-			
-			// var_dump($this->db->buildSelect());
+		return $result;
+	}
 
-			return $result;
-		}
+	public function stockList($warehouse, $sort, $category, $startdate, $enddate){
+		$result	= $this->getQueryDetails($warehouse, $sort, $category, $startdate, $enddate)
+						->runPagination();
+		return $result;
+		
+	}
 
-		public function getValue($table, $cols = array(), $cond, $orderby = "", $bool = "")
-		{
-			$result = $this->db->setTable($table)
-						->setFields($cols)
-						->setWhere($cond)
-						->setOrderBy($orderby)
-						->runSelect($bool)
+	public function fileExport($warehouse, $sort, $category, $startdate, $enddate){
+		$result	= $this->getQueryDetails($warehouse, $sort, $category, $startdate, $enddate)
+						->runSelect()
 						->getResult();
+		return $result;
+	}
 
-			return $result;
-		}
+	public function retrieveData($table, $fields = array(), $cond = "", $join = "", $orderby = "", $groupby = ""){
+		$result = $this->db->setTable($table)
+					->setFields($fields)
+					->leftJoin($join)
+					->setGroupBy($groupby)
+					->setWhere($cond)
+					->setOrderBy($orderby)
+					->runSelect()
+					->getResult();
+		return $result;
+	}
+
+	public function getValue($table, $cols = array(), $cond, $orderby = "", $bool = ""){
+		$result = $this->db->setTable($table)
+					->setFields($cols)
+					->setWhere($cond)
+					->setOrderBy($orderby)
+					->runSelect($bool)
+					->getResult();
+
+		return $result;
+	}
 
 	public function customer_list($itemcode, $startdate, $enddate) {
 		$condition = '';
@@ -124,7 +83,6 @@ class sales_stock extends wc_model {
 						->setGroupBy(" cust.partnercode ")
 						->setOrderBy(" inv.voucherno DESC ")
 						->runPagination();
-						// echo $this->db->getQuery();
 		return $result;
 	}
 
@@ -136,4 +94,79 @@ class sales_stock extends wc_model {
 		return '(' . implode(' OR ', $temp) . ')';
 	}
 
+	public function getSalesTotal($warehouse, $sort, $category, $startdate, $enddate) {
+		$result	= $this->getQueryDetails($warehouse, $sort, $category, $startdate, $enddate)
+						->setGroupBy('')
+						->setOrderBy('')
+						->runSelect()
+						->getRow();
+
+		return $result;
+	}
+
+	private function getQueryDetails($warehouse, $sort, $category, $startdate, $enddate){
+		$condition = "";
+		$condition .= (!empty($warehouse) && $warehouse != 'none' )? " AND warehouse = '$warehouse' ":"";
+		$condition .= (!empty($startdate) && !empty($enddate)) ? " AND iq.transactiondate BETWEEN '$startdate' AND '$enddate' " : "";
+		$condition .= (!empty($category) && $category != 'none') ? "AND ic.id = '$category' " : "";
+		$sort = (!empty($sort)) ? str_replace('+', ' ', $sort) : 'sorter, parent, classid';
+		
+		if ($category == '' || $category == 'none') {
+			$category = 0;
+		}
+
+		$si = $this->db->setTable('salesinvoice a')
+						->innerJoin('salesinvoice_details b ON a.companycode = b.companycode AND a.voucherno = b.voucherno')
+						->setFields("itemcode, transactiondate, a.companycode, a.customer, b.warehouse, issueqty sales, 0 returns, b.amount + b.taxamount - b.itemdiscount amount, 0 return_amount, b.unitprice price, issueuom uom")
+						->setWhere("a.stat = 'open' OR a.stat = 'posted'")
+						->buildSelect();
+
+		$sr = $this->db->setTable('salesreturn a')
+						->innerJoin('salesreturn_details b ON a.companycode = b.companycode AND a.voucherno = b.voucherno')
+						->setFields("itemcode, transactiondate, a.companycode, a.customer, b.warehouse, 0 sales, issueqty returns, 0 amount, b.amount + b.taxamount return_amount, b.unitprice price, issueuom uom")
+						->setWhere("a.stat = 'Returned'")
+						->buildSelect();
+
+		$inner_query = $si . ' UNION ALL ' . $sr;
+
+		$classes = $this->getItemClassList($category);
+
+		$query = $this->db->setTable("($classes) ic")
+							->leftJoin('items i ON i.classid = ic.id AND i.companycode = ic.companycode')
+							->leftJoin("($inner_query) iq ON iq.itemcode = i.itemcode AND iq.companycode = i.companycode")
+							->setFields('ic.id classid, i.itemcode, i.itemname detailparticular, warehouse, label, FORMAT((SUM(sales) - SUM(returns)),0) issueqty, SUM(price) unitprice,  (SUM(amount)-SUM(return_amount)) amount, uom')
+							->setWhere("warehouse != '' AND ((transactiondate >= '$startdate' AND transactiondate <= '$enddate'))  AND iq.itemcode IS NOT NULL" . $condition)
+							->setGroupBy('i.itemcode')
+							->setOrderBy($sort);
+
+		return $query;
+	}
+
+	public function getItemClassList($category) {
+		$result = $this->db->setTable('itemclass ic')
+							->leftJoin('itemclass p ON ic.parentid = p.id AND ic.companycode = p.companycode')
+							->setFields('ic.id id, ic.label label, ic.parentid parentid, ic.companycode companycode')
+							->runSelect()
+							->getResult();
+
+		return implode(' UNION ALL ', $this->buildTree($result, $category));
+	}
+
+	private function buildTree($list, $pid = 0, $level_temp = 0, &$linenum = 0, $addself = true) {
+		$op = array();
+		$level = $level_temp;
+		$level++;
+		foreach($list as $item) {
+		  if ($item->parentid == $pid || ($addself && $item->id == $pid)) {
+			$linenum++;
+			$sorter = ($item->parentid == 0) ? $item->id : $item->parentid;
+			$op[] = "SELECT {$item->id} id, '{$item->label}' label, $level level, '{$item->companycode}' companycode, $linenum linenum, '$item->parentid' parent, '$sorter' sorter";
+			if ( ! ($addself && $item->id == $pid)) {
+			  $children = $this->buildTree($list, $item->id, $level, $linenum, false);
+			  $op = array_merge($op, $children);
+			}
+		  }
+		}
+		return $op;
+	}
 }
