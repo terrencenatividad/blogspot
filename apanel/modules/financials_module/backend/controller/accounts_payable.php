b4b30f15ef95653d1bf42815436ec04209b53678
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-08-17 10:08:55 +0800

    AP : Fix on Tab Filter

--- apanel/modules/financials_module/backend/controller/accounts_payable.php
+++ apanel/modules/financials_module/backend/controller/accounts_payable.php
@@ -701,34 +701,46 @@ class controller extends wc_controller
 					}
 				}
 				
-				$balance     = $row->balance; 
-				$amount	  	 = $row->amount; 
-				$vendor		 = $row->vendor; 
-				$referenceno = $row->referenceno; 
-				$checker 	 = $row->importchecker;
-				$import 	 = ($checker == 'import') 	?	"Yes" 	:	"No";
-				$stat		 = $row->stat;
+				$balance     		= $row->balance; 
+				$amount	  	 		= $row->amount; 
+				$vendor		 		= $row->vendor; 
+				$referenceno 		= $row->referenceno; 
+				$checker 	 		= $row->importchecker;
+				$import 	 		= ($checker == 'import') 	?	"Yes" 	:	"No";
+				$stat				= $row->stat;
+				$payment_status 	= $row->payment_status;
 
-				if($balance != 0 && $stat == 'cancelled')
-				{
+				// if($balance != 0 && $stat == 'cancelled')
+				// {
+				// 	$voucher_status = '<span class="label label-danger">CANCELLED</span>';
+				// }
+				// else if($balance != $amount && $balance != 0 && $stat == 'cancelled')
+				// {
+				// 	$voucher_status = '<span class="label label-danger">CANCELLED</span>';
+				// }
+				// else if($balance != $amount && $balance != 0)
+				// {
+				// 	$voucher_status = '<span class="label label-info">PARTIAL</span>';
+				// }
+				// else if($balance != 0)
+				// {
+				// 	$voucher_status = '<span class="label label-warning">UNPAID</span>';
+				// }
+				// else
+				// {
+				// 	$voucher_status = '<span class="label label-success">PAID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
+				// }
+
+				if($payment_status == "paid") {
+					$voucher_status = '<span class="label label-success">PAID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
+				} else if($payment_status == 'unpaid'){
+					$voucher_status = '<span class="label label-warning">UNPAID</span>';
+				} else if($payment_status == 'cancelled'){
 					$voucher_status = '<span class="label label-danger">CANCELLED</span>';
-				}
-				else if($balance != $amount && $balance != 0 && $stat == 'cancelled')
-				{
-					$voucher_status = '<span class="label label-danger">CANCELLED</span>';
-				}
-				else if($balance != $amount && $balance != 0)
-				{
+				} else if($payment_status == "partial"){
 					$voucher_status = '<span class="label label-info">PARTIAL</span>';
 				}
-				else if($balance != 0)
-				{
-					$voucher_status = '<span class="label label-warning">UNPAID</span>';
-				}
-				else
-				{
-					$voucher_status = '<span class="label label-success">PAID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>';
-				}
+
 				$show_edit 		= ($balance == $amount  && $stat != 'cancelled');
 				$show_delete 	= ($balance == $amount && $stat != 'cancelled');
 				$show_payment 	= ($balance != 0  && $stat != 'cancelled');
