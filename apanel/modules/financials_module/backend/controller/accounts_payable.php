07bdbba4905293eb77185a006588f72571044dbd
Mark Tamo <mark.tamo@cid-systems.com>
2018-08-29 14:25:22 +0800

    Financials > AP : Import support for windows libre office

--- apanel/modules/financials_module/backend/controller/accounts_payable.php
+++ apanel/modules/financials_module/backend/controller/accounts_payable.php
@@ -1186,7 +1186,7 @@ class controller extends wc_controller
 
 		$filedir	= $_FILES["file"]["tmp_name"];
 
-		$file_types = array( "text/x-csv","text/tsv","text/comma-separated-values", "text/csv", "application/csv", "application/excel", "application/vnd.ms-excel", "application/vnd.msexcel", "text/anytext");
+		$file_types = array( "text/x-csv","text/tsv","text/comma-separated-values", "text/csv", "application/csv", "application/excel", "application/vnd.ms-excel", "application/vnd.msexcel", "text/anytext", "application/octet-stream");
 
 		$errmsg 	=	array();
 		$proceed 	=	false;
@@ -1203,6 +1203,7 @@ class controller extends wc_controller
 		
 		$headerArr = array('Document Set','Transaction Date','Due Date','Supplier Code','Invoice No.','Reference No.','Notes','Account Name','Description','Debit','Credit');
 
+		$warning 			=	array();
 		if( empty($errmsg) ) {
 			$x = array_map('str_getcsv', file($_FILES['file']['tmp_name']));
 			$error 	=	array();
@@ -1235,7 +1236,6 @@ class controller extends wc_controller
 			}
 			$line 				=	2;
 			$post 				=	array();
-			$warning 			=	array();
 			$vouchlist 			= 	array();
 			$h_vouchlist 		=	array();
 			$datelist 			= 	array();
