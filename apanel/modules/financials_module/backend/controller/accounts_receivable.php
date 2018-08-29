f6eeff7fa2c13f620b5e548d38d3b9dea9bc9a83
Mark Tamo <mark.tamo@cid-systems.com>
2018-08-29 14:38:26 +0800

    Financials > JV / AR : Import support for windows libre office

--- apanel/modules/financials_module/backend/controller/accounts_receivable.php
+++ apanel/modules/financials_module/backend/controller/accounts_receivable.php
@@ -1063,7 +1063,7 @@ class controller extends wc_controller
 
 		$filedir	= $_FILES["file"]["tmp_name"];
 
-		$file_types = array( "text/x-csv","text/tsv","text/comma-separated-values", "text/csv", "application/csv", "application/excel", "application/vnd.ms-excel", "application/vnd.msexcel", "text/anytext");
+		$file_types = array( "text/x-csv","text/tsv","text/comma-separated-values", "text/csv", "application/csv", "application/excel", "application/vnd.ms-excel", "application/vnd.msexcel", "text/anytext", "application/octet-stream");
 
 		$errmsg 	=	array();
 		$proceed 	=	false;
@@ -1079,7 +1079,7 @@ class controller extends wc_controller
 		}
 		
 		$headerArr = array('Document Set','Transaction Date','Due Date','Customer Code','Invoice No.','Reference No.','Notes','Account Name','Description','Debit','Credit');
- 
+		$warning 			=	array();
 		if( empty($errmsg) ) {
 			$x = array_map('str_getcsv', file($_FILES['file']['tmp_name']));
 			$error 	=	array();
@@ -1107,7 +1107,6 @@ class controller extends wc_controller
 			
 			$line 				=	2;
 			$post 				=	array();
-			$warning 			=	array();
 			$vouchlist 			= 	array();
 			$h_vouchlist 		=	array();
 			$datelist 			= 	array();
--- apanel/modules/financials_module/backend/controller/journal_voucher.php
+++ apanel/modules/financials_module/backend/controller/journal_voucher.php
@@ -243,7 +243,7 @@ class controller extends wc_controller {
 
 		$filedir	= $_FILES["file"]["tmp_name"];
 
-		$file_types = array( "text/x-csv","text/tsv","text/comma-separated-values", "text/csv", "application/csv", "application/excel", "application/vnd.ms-excel", "application/vnd.msexcel", "text/anytext");
+		$file_types = array( "text/x-csv","text/tsv","text/comma-separated-values", "text/csv", "application/csv", "application/excel", "application/vnd.ms-excel", "application/vnd.msexcel", "text/anytext", "application/octet-stream");
 
 		$errmsg 	=	array();
 		$proceed 	=	false;
@@ -259,7 +259,8 @@ class controller extends wc_controller {
 		}
 		
 		$headerArr = array('Document Set','Transaction Date','Reference','Notes','Account Name','Description','Debit','Credit');
- 
+		
+		$warning 			=	array();
 		if( empty($errmsg) ) {
 			$x = array_map('str_getcsv', file($_FILES['file']['tmp_name']));
 			$error 	=	array();
@@ -287,7 +288,6 @@ class controller extends wc_controller {
 			
 			$line 				=	2;
 			$post 				=	array();
-			$warning 			=	array();
 			$vouchlist 			= 	array();
 			$h_vouchlist 		=	array();
 			$datelist 			= 	array();
