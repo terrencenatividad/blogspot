d59cbc4761efff11b36c72091fe8b72e4622ced6
marion <marion.rosales@cid-systems.com>
2018-09-05 18:18:14 +0800

    Fixed Asset

---
+++ apanel/modules/maintenance_module/backend/view/fixed_asset/fixed_asset_category.php
@@ -0,0 +1,72 @@
+<section class="content">
+	<div class = "alert alert-warning alert-dismissable hidden">
+		<button type="button" class="close" data-dismiss="alert">×</button>
+		<h4><strong>Error!<strong></h4>
+		<div id = "errmsg"></div>
+	</div>
+
+	<div class="box box-primary">
+		<div class="panel panel-default">
+			
+		<form class="form-horizontal form-group" method="POST" id="fixed_asset_form" autocomplete="off">
+			<div class="panel-body">
+				<div class="row">
+				
+					<div class="col-md-12">		
+					<?
+						echo $ui->formField('text')
+									->setLabel('Description ')
+									->setSplit('col-md-4', 'col-md-4')
+									->setName('description')
+									->setId('description')
+									->setValue($description)
+									->setValidation('required')
+									->draw($show_input);
+					?>
+					</div>				
+				</div>
+			</div>
+			<div class="panel-footer">
+				<div class="row center">
+					<div class="col-md-5 col-sm-4 col-xs-4"></div>
+					<?php echo $ui->drawSubmit($show_input); ?>
+						<a href="<?=BASE_URL?>maintenance/fixed_asset/listing_category" class="btn btn-default">Cancel</a>
+                        </div>
+			</div>
+		</form>
+	</div>
+	</div>
+</section>
+<script>
+
+$('form').submit(function(e) 
+{
+		e.preventDefault();
+		var form_group	 	= 	$('#fixed_asset_form #fixed_asset_form').closest('.form-group');
+		$('#fixed_asset_form').find('.form-group').find('input, textarea, select').trigger('blur');
+
+		if ($('#fixed_asset_form').find('.form-group.has-error').length == 0)
+		{	
+			$.post('<?=MODULE_URL?>ajax/<?=$ajax_task?>', $(this).serialize() + '<?=$ajax_post?>', function(data) {
+				if( data.msg == 'success' )
+				{
+					$('#delay_modal').modal('show');
+						setTimeout(function() {							
+						window.location =  "<?=MODULE_URL?>listing_category";					
+					}, 1000)
+				}
+				if(data.msg == "error_add")
+				{
+					$(".alert-warning").removeClass("hidden");
+					$("#errmsg").html("Duplicate Entry for ATC Code : " + 
+										data.itemno + " Tax account was already used : " + data.description  );
+				}
+			});
+			
+		}else{
+				if (form_group.find('p.help-block').html() != "") {
+					form_group.removeClass('has-error').find('p.help-block').html('');
+				}
+		}
+});
+</script>
