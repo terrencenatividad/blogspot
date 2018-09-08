9990af951f92ec8734ed484c14c991e7a848d20d
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-09-08 16:28:25 +0800

    RV: Fix on Customer Change

--- apanel/modules/financials_module/backend/view/receipt_voucher/receipt_voucher.php
+++ apanel/modules/financials_module/backend/view/receipt_voucher/receipt_voucher.php
@@ -66,7 +66,7 @@
 								->addHidden(($task == 'view'))
 								->draw($show_input);
 								?>
-								<input id="old_customer" type="hidden">
+								<input id="new_customer" type="hidden">
 							</div>
 							<div class = "col-md-6">
 								<?php
@@ -3924,13 +3924,27 @@ $(document).ready(function() {
 		drawTemplate();
 	});
 
+	$('#customer').on('select2:selecting', function(e){
+		console.log(e);
+		var accounts_selected 	= computefortotalaccounts();
+		if(accounts_selected > 0){
+			e.preventDefault();
+			$('#change_customer_modal').modal('show');
+			$(this).select2('close');
+		}
+		var new_customer = e.params.args.data.id;
+		$('#new_customer').val(new_customer);
+	});	
+
 	$('#change_customer_modal').on('click','#no_to_reset',function(){
 		
-		$(this).modal('hide');
+		$('#change_customer_modal').modal('hide');
 	});
 	
 	$('#change_customer_modal').on('click','#yes_to_reset',function(){
-		
+		var customer = $('#new_customer').val();
+		$('#customer').val(customer).trigger('change');
+
 		$('#ap_items .clone').each(function(index) {
 			if (index > 0) {
 				$(this).remove();
@@ -3955,13 +3969,9 @@ $(document).ready(function() {
 	var customer = $('#customer').val();
 
 	$('#customer').on('change',function(){
-		var accounts_selected 	= computefortotalaccounts();
 		var total_payment 		= $('#total_payment').val();
 		var total_discount 		= $('#total_discount').val();
 
-		if(accounts_selected > 0){
-			$('#change_customer_modal').modal('show');
-		}
 		// Get Customer Credit
 		var customer = $(this).value;
 
