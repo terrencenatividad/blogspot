e699a9ef8c9b69ab5f07ac7b9bdfd8e4742199b3
Earvin Jake Agamata <earvin.agamata@cid-systems.com>
2018-09-11 14:26:24 +0800

    Payment Voucher: Update Cancel Confirmation Message

--- apanel/modules/financials_module/backend/view/payment_voucher/payment_voucher.php
+++ apanel/modules/financials_module/backend/view/payment_voucher/payment_voucher.php
@@ -1198,7 +1198,7 @@
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
 			</div>
 			<div class="modal-body">
-				Are you sure you want to Cancel this Transaction?
+				Are you sure you want to cancel this transaction?
 			</div>
 			<div class="modal-footer">
 				<div class="row row-dense">
