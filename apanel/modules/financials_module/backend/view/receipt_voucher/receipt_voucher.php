3a341f9354d2283276db43732056d816a6f9ac81
Earvin Jake Agamata <earvin.agamata@cid-systems.com>
2018-09-11 14:26:31 +0800

    Reciept Voucher: Update Cancel Confirmation Message

--- apanel/modules/financials_module/backend/view/receipt_voucher/receipt_voucher.php
+++ apanel/modules/financials_module/backend/view/receipt_voucher/receipt_voucher.php
@@ -1029,7 +1029,7 @@
 				<button type="button" class="close" data-dismiss="modal">&times;</button>
 			</div>
 			<div class="modal-body">
-				Are you sure you want to cancel?
+				Are you sure you want to cancel this transaction?
 			</div>
 			<div class="modal-footer">
 				<div class="row row-dense">
