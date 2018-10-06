5d47178b293f2b31ff7182be764501b1a3a6e7e1
marion <marion.rosales@cid-systems.com>
2018-10-06 14:58:53 +0800

    Import ATC EWT, CWT

--- apanel/modules/maintenance_module/backend/controller/atc_code.php
+++ apanel/modules/maintenance_module/backend/controller/atc_code.php
@@ -94,6 +94,8 @@
 			$data["account_list"] = $this->atc_code->getValue("chartaccount", 
 			array("id ind","CONCAT(segment5, ' - ', accountname) val"), " accountclasscode = 'TAX' ", "",false,false);
 
+			$data["s_account_list"] = $this->atc_code->getValue("chartaccount", 
+			array("id ind","CONCAT(segment5, ' - ', accountname) val"), " accountname = 'Creditable Withholding Tax'", "",false,false);
 
 			$list = $this->atc_code->retrieveData($retdata,"","",$condition);
 		
@@ -160,7 +162,7 @@
 				$errmsg[]= "Invalid file type, file must be .csv.<br/>";
 			}
 
-			$headerArr = array('ATC Code','Tax Rate', 'Tax Code', 'Description', 'Tax Account');
+			$headerArr = array('ATC Code','Tax Rate', 'Tax Code', 'Description', 'EWT','CWT');
 			
 			if( empty($errmsg) )
 			{
@@ -204,7 +206,8 @@
 						$taxrate 	   	= $b[1];
 						$taxcode 	   	= $b[2];
 						$description   	= $b[3];
-						$taxaccount		= $b[4];
+						$ewt			= $b[4];
+						$cwt			= $b[5];
 
 						$exists = $this->atc_code->check_duplicate($atccode);
 						$count = $exists[0]->count;
@@ -219,19 +222,23 @@
 						}
 						
 						if(empty($atccode)){
-							$errmsg[] 	= "ATC Code is required. Row $line should not be empty.<br>";
+							$errmsg[] 	= "ATC Code on row $line should not be empty.<br>";
 						}
 					
 						if(empty($taxrate)){
-							$errmsg[] 	= "Tax rate is required. Row $line should not be empty.<br>";
+							$errmsg[] 	= "Tax rate on row $line should not be empty.<br>";
 						}
 		
-						if(empty($taxaccount)){
-							$errmsg[] 	= "Tax account is required. Row $line should not be empty.<br>";
+						if(empty($ewt)){
+							$errmsg[] 	= "EWT on row $line should not be empty.<br>";
+						}
+
+						if(empty($cwt)){
+							$errmsg[] 	= "CWT on row $line should not be empty.<br>";
 						}
 						
 						if(empty($description)){
-							$errmsg[] 	= "Tax description is required. Row $line should not be empty.<br>";
+							$errmsg[] 	= "Tax description on row $line should not be empty.<br>";
 						}
 					
 						
@@ -239,7 +246,8 @@
 						$taxrate_[] 	= $taxrate/100;
 						$taxcode_[] 	= $taxcode;
 						$description_[]	= $description;
-						$taxaccount_[] 	= $taxaccount;
+						$ewt_[] 		= $ewt;
+						$cwt_[] 		= $cwt;
 
 						$line++;
 				}
@@ -253,7 +261,8 @@
 						'tax_rate'			=> $taxrate_,
 						'wtaxcode'			=> $taxcode_,
 						'short_desc'		=> $description_,
-						'tax_account'		=> $taxaccount_
+						'tax_account'		=> $ewt_,
+						'cwt'				=> $cwt_,
 					);
 					
 					$proceed  				= $this->atc_code->importATC($post);
@@ -276,7 +285,7 @@
 			$sort 	= $this->input->post("sort");
 			$limit 	= $this->input->post("limit");
 			$addCond = stripslashes($this->input->post("addCond"));
-			$fields = array("atcId","atc_code","tax_rate","wtaxcode","atc_type","short_desc","tax_account","accountname","a.stat stat");
+			$fields = array("atcId","atc_code","tax_rate","wtaxcode","atc_type","short_desc","tax_account","cwt","accountname","a.stat stat");
 			$table = "";
 			$list = $this->atc_code->retrieveData($fields,$search, $sort, $addCond, $limit); 
 	
@@ -320,6 +329,7 @@
 								<td>'.$row->wtaxcode.'</td> 
 								<td>'.$row->short_desc.'</td>
 								<td>'.$row->accountname.'</td>
+								<td>'.'Creditable Withholding Tax'.'</td>
 								<td>'.$status.'</td>'; 
 							'</tr>';			
 				}
