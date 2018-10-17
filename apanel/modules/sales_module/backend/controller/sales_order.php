fe0a868a0a4ac1a541c9838b11429742b888e8ad
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-10-17 11:35:00 +0800

    SO: Update on credit limit from sq -> so

--- apanel/modules/sales_module/backend/controller/sales_order.php
+++ apanel/modules/sales_module/backend/controller/sales_order.php
@@ -107,7 +107,11 @@ class controller extends wc_controller
 		/** RETRIEVAL FOR QUOTATION / SO **/
 
 		$data["quotation_no"]  = ""; 
-
+		$data['h_curr_limit'] 	= 0;
+		$data['h_outstanding'] 	= 0;
+		$data['h_incurred']  	= 0;
+		$data['h_balance'] 		= 0;
+		
 		if( $quotation_no != "" )
 		{
 			$retrieved_data 		= $this->so->retrieveExistingSQ($quotation_no);
@@ -156,11 +160,6 @@ class controller extends wc_controller
 			$data['h_incurred']  	= $incurred;
 			$data['h_balance'] 		= (($credit_limit - $outstanding) > 0) ? $credit_limit - $outstanding 	: 0;
 		}
-
-		$data['h_curr_limit'] 	= 0;
-		$data['h_outstanding'] 	= 0;
-		$data['h_incurred']  	= 0;
-		$data['h_balance'] 		= 0;
 	
 		$data['ui'] 			= $this->ui;
 		$data['show_input'] 	= true;		
