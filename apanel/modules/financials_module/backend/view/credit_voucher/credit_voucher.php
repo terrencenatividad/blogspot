5815bc912f87e1ae27e0944c367e314a5135e6c2
Juliesse Rasco <juliesse>
2018-10-18 09:09:18 +0800

    CV: Validations

--- apanel/modules/financials_module/backend/view/credit_voucher/credit_voucher.php
+++ apanel/modules/financials_module/backend/view/credit_voucher/credit_voucher.php
@@ -69,6 +69,7 @@
 											->setSplit('col-md-4', 'col-md-8')
 											->setName('referenceno')
 											->setId('referenceno')
+											->setMaxLength(30)
 											->setValue($referenceno)
 											->draw($show_input);
 											?>
@@ -190,6 +191,20 @@
 				<?php if ($show_input): ?>
 					var ajax		= {};
 					var ajax_call	= '';
+					$(document).ready(function() {
+						$("#amount").keydown(function (e) {
+							if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+								(e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+								(e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+								(e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+								(e.keyCode >= 35 && e.keyCode <= 39)) {
+									return;
+							}
+							if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+								e.preventDefault();
+							}
+						});
+					});
 					$('#customer').on('change', function() {
 						$('#invoiceno').val('');
 						$('#receivableno').val('');
