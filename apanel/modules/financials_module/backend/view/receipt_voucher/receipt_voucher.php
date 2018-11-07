12fd364d079f4f7f16020334240a3b3023975ff1
Jasmine Ang <jasmine.ang@cid-systems.com>
2018-11-07 09:26:04 +0800

    RV: Update on disabled buttons

--- apanel/modules/financials_module/backend/view/receipt_voucher/receipt_voucher.php
+++ apanel/modules/financials_module/backend/view/receipt_voucher/receipt_voucher.php
@@ -99,7 +99,7 @@
 											if(!$show_input){
 												echo '<p class="form-control-static">'.number_format($sum_applied,2).'</p>';
 											}else{
-												$disable_receivables 	=	($ap_checker == 'yes') 	? "disabled" 	:	"";
+												$disable_receivables 	=	($ap_checker == 1) 	? "disabled" 	:	"";
 												?>
 												<button type="button" id="apv" class="btn btn-block btn-success btn-flat" <?=$disable_receivables?>>
 													<em class="pull-left"><small>Click to view tagged receivables</small></em>
@@ -140,7 +140,7 @@
 											if(!$show_input){
 												echo '<p class="form-control-static">'.number_format($credits_applied,2).'</p>';
 											}else{
-												$disable_cv 	=	($ap_checker == 'yes') 	? "disabled" 	:	"";
+												$disable_cv 	=	($ap_checker == 1) 	? "disabled" 	:	"";
 												?>
 												<button type="button" id="crv" class="btn btn-block btn-success btn-flat" <?=$disable_cv?>>
 													<em class="pull-left"><small>Click to view tagged credits</small></em>
@@ -209,8 +209,7 @@
 															->setId("cwt")
 															->setName("cwt")
 															->setClass("cwt")
-															->setDefault("")
-															->setValue(1)
+															->setDefault('1')
 															->setAttribute(array("disabled" => "disabled"))
 															->draw($show_input);
 												}else{
