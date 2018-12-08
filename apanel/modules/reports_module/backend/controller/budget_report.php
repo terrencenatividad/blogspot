c9c898c98fdfbbce49f892ab4fbb0663d03323c8
terensnatividad <terrence.natividad@cid-systems.com>
2018-12-08 10:13:03 +0800

    Budget Report : module

---
+++ apanel/modules/reports_module/backend/controller/budget_report.php
@@ -0,0 +1,84 @@
+<?php
+class controller extends wc_controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->ui					= new ui();
+		$this->input				= new input();
+		$this->budget_report				= new budget_report_model();
+		$this->view->header_active	= 'report/';
+	}
+
+	public function view() {
+		$this->view->title		= 'Budget Variance Report';
+		$data['ui']				= $this->ui;
+		$data['datefilter']		= date("M d, Y");
+		$data['costcenter_list']	= $this->budget_report->getCostCenterList();
+		$this->view->load('budget_report', $data);
+	}
+
+	public function ajax($task) {
+		$ajax = $this->{$task}();
+		if ($ajax) {
+			header('Content-type: application/json');
+			echo json_encode($ajax);
+		}
+	}
+
+	private function ajax_list() {
+		$data			= $this->input->post(array('costcenter', 'budget_type'));
+		extract($data);
+
+		$pagination		= $this->budget_report->getBudgetList($costcenter, $budget_type);
+
+		$table		= '';
+		if (empty($pagination->result)) {
+			$table = '<tr><td colspan="9" class="text-center"><b>No Records Found</b></td></tr>';
+		}
+		foreach ($pagination->result as $key => $row) {
+			$table .= '<tr>';
+			$table .= '<td>' . $row->accountcode . '</td>';
+			$table .= '<td>' . $row->description . '</td>';
+			$table .= '<td class = "amount">' . number_format($row->amount, 2) . '</td>';
+			$table .= '<td class = "actual">' . number_format($row->actual, 2) . '</td>';
+			$table .= '<td class = "variance">' . number_format($row->variance, 2) . '</td>';
+			$table .= '</tr>';
+		}
+
+		$pagination->table	= $table;
+		$pagination->csv	= $this->get_export();
+		return $pagination;
+	}
+
+	private function get_export() {
+		$data			= $this->input->post(array('costcenter', 'budget_type'));
+		extract($data);
+
+		$result			= $this->budget_report->getBudgetReportExport($costcenter, $budget_type);
+
+		$header = array(
+			'Account Code',
+			'Description',
+			'Budget',
+			'Actual',
+			'Variance'
+		);
+
+		$csv = '';
+		$csv .= '"' . implode('","', $header) . '"';
+		if (empty($result)) {
+			$csv .= 'No Records Found';
+		}
+		foreach ($result as $key => $row) {
+			$csv .= "\n";
+			$csv .= '"' . $row->accountcode . '",';
+			$csv .= '"' . $row->description . '",';
+			$csv .= '"' . $row->amount . '",';
+			$csv .= '"' . $row->actual . '",';
+			$csv .= '"' . $row->variance . '",';
+		}
+		
+		return $csv;
+	}
+}
+?>
