611238e12ccbe059722737c303779b930e379857
terensnatividad <terrence.natividad@cid-systems.com>
2019-01-18 16:31:31 +0800

    Exchange rate : inactivate activate

--- apanel/modules/maintenance_module/backend/view/exchange_rate/exchange_rate_list.php
+++ apanel/modules/maintenance_module/backend/view/exchange_rate/exchange_rate_list.php
@@ -1,19 +1,19 @@
 <section class="content">
-    <div class="box box-primary">
-        
-        <div class="box-header">
-            <div class="row">
+	<div class="box box-primary">
 
-            <div class = "col-md-8">
+		<div class="box-header">
+			<div class="row">
+
+				<div class = "col-md-8">
 					<?= 
-						$ui->CreateNewButton('');
+					$ui->CreateNewButton('');
 					?>
 					<?=	$ui->CreateDeleteButton(''); ?>
 					<?=	$ui->CreateActButton(''); ?>
 
                     <!-- <a href="<?php echo BASE_URL; ?>maintenance/exchange_rate/create" class = "btn btn-primary danger">Create</a>
-					<button type="button" id="item_multiple_delete" class="btn btn-danger delete_button">Delete <span></span> </button> -->
-					
+                    	<button type="button" id="item_multiple_delete" class="btn btn-danger delete_button">Delete <span></span> </button> -->
+
 					<!--<div class="btn btn-group" id="option_buttons">
 						<button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown">
 							Options <span class="caret"></span>
@@ -29,15 +29,15 @@
 					</div>-->
 				</div>
 
-                <div class="col-md-4 pull-right">
-                    <div class="input-group input-group-sm">
-                        <input id="search" name="table_search" class="form-control pull-right" placeholder="Search" type="text">
-                        <div class="input-group-btn">
-                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="col-md-4 pull-right">
+					<div class="input-group input-group-sm">
+						<input id="search" name="table_search" class="form-control pull-right" placeholder="Search" type="text">
+						<div class="input-group-btn">
+							<button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+						</div>
+					</div>
+				</div>
+			</div>
 			<br>
 
 			<div class="row">
@@ -74,11 +74,11 @@
 				</div>
 			</div>
 
-        </div>
+		</div>
 
-       	<div class="box-body table table-responsive">
-            <table id = "exchangerate_table" class="table table-hover">
-                <thead>
+		<div class="box-body table table-responsive">
+			<table id = "exchangerate_table" class="table table-hover">
+				<thead>
 					<!--<tr class = "info">
 						<th class = "col-md-1 text-center">
                             <input type = "checkbox" class = "checkall"/>
@@ -87,31 +87,31 @@
                         <th class = "col-md-3 text-center">Base Currency Code</th>
                         <th class = "col-md-3 text-center">Exchange Currency Code</th>
                         <th class = "col-md-3 text-center">Exchange Rate</th>
-					</tr>-->
+                    </tr>-->
 
-					<?php
-						echo $ui->loadElement('table')
-								->setHeaderClass('info')
-								->addHeader(
-									'<input type="checkbox" class="checkall">',
-									array(
-										'class' => 'col-md-1 text-center'
-									)
-								)
-								->addHeader('Effectivity Date',array('class'=>'col-md-3'),'sort','effectivedate')
-								->addHeader('Base Currency Code', array('class'=>'col-md-3'),'sort','basecurrencycode')
-								->addHeader('Exchange Currency Code', array('class'=>'col-md-3'),'sort','exchangecurrencycode')
-								->addHeader('Exchange Rate', array('class'=>'col-md-3'),'sort','exchangerate')
-								->addHeader('Status', array('class'=>'col-md-3'),'sort','stat')
-								->draw();
-					?>
-				</thead>
-               
+                    <?php
+                    echo $ui->loadElement('table')
+                    ->setHeaderClass('info')
+                    ->addHeader(
+                    	'<input type="checkbox" class="checkall">',
+                    	array(
+                    		'class' => 'col-md-1 text-center'
+                    	)
+                    )
+                    ->addHeader('Effectivity Date',array('class'=>'col-md-3'),'sort','effectivedate')
+                    ->addHeader('Base Currency Code', array('class'=>'col-md-3'),'sort','basecurrencycode')
+                    ->addHeader('Exchange Currency Code', array('class'=>'col-md-3'),'sort','exchangecurrencycode')
+                    ->addHeader('Exchange Rate', array('class'=>'col-md-3'),'sort','exchangerate')
+                    ->addHeader('Status', array('class'=>'col-md-3'),'sort','stat')
+                    ->draw();
+                    ?>
+                </thead>
+
                 <tbody id = "list_container">
-				</tbody>
+                </tbody>
 
             </table>
-			<div id="pagination"></div>
+            <div id="pagination"></div>
         </div>   
 
     </div>
@@ -119,141 +119,141 @@
 
 
 <script>
-var ajax = {};
+	var ajax = {};
 
-function show_error(msg)
-{
-	$(".delete-modal").modal("hide");
-	$(".alert-warning").removeClass("hidden");
-	$("#errmsg").html(msg);
-}
-
-function showList(pg){
-	$.post('<?=BASE_URL?>maintenance/exchange_rate/ajax/exchange_rate_list', ajax, function(data)
+	function show_error(msg)
 	{
-		$('#exchangerate_table #list_container').html(data.table);
-        $('#pagination').html(data.pagination);
-		historyOfMyLife();
-        //$("#export").attr('href', 'data:text/csv;filename=testing.csv;charset=utf-8,' + encodeURIComponent(data.csv));
-		if (ajax.page > data.page_limit && data.page_limit > 0) {
-			ajax.page = data.page_limit;
-			showList();
-		}
-	});
-};
-
-$( "#search" ).keyup(function() 
-{
-	var search = $( this ).val();
-	ajax.search = search;
-	showList();
-});
-
-$('#pagination').on('click', 'a', function(e) {
-	e.preventDefault();
-	$('.checked').iCheck('uncheck');
-	var li = $(this).closest('li');
-	if (li.not('.active').length && li.not('.disabled').length) {
-		ajax.page = $(this).attr('data-page');
-		showList();
+		$(".delete-modal").modal("hide");
+		$(".alert-warning").removeClass("hidden");
+		$("#errmsg").html(msg);
 	}
-});
 
-$(document).ready(function() 
-{
-	showList();
-
-	$( "#exchangerate_table" ).on('click' , '.delete', function() 
-	{
-		var id = $( this ).attr("data-id");
-		
-		if( id != "" )
+	function showList(pg){
+		$.post('<?=BASE_URL?>maintenance/exchange_rate/ajax/exchange_rate_list', ajax, function(data)
 		{
-			$(".delete-modal > .modal").css("display", "inline");
-			$(".delete-modal").modal("show");
+			$('#exchangerate_table #list_container').html(data.table);
+			$('#pagination').html(data.pagination);
+			historyOfMyLife();
+        //$("#export").attr('href', 'data:text/csv;filename=testing.csv;charset=utf-8,' + encodeURIComponent(data.csv));
+        if (ajax.page > data.page_limit && data.page_limit > 0) {
+        	ajax.page = data.page_limit;
+        	showList();
+        }
+    });
+	};
 
-			$( "#delete-yes" ).click(function() 
-			{
-				$.post('<?=BASE_URL?>maintenance/exchange_rate/ajax/delete', 'id=' + id, function(data) 
-				{
-					if( data.msg == 'success' )	
-					{
-						$(".delete-modal").modal("hide");
-						showList();
-					}
-					else
-					{			
-						$(".delete-modal").modal("hide");
-						show_error("Unable to delete the Exchange Rate(s).");
-					}
-				});
-			});	
-		}
-
-	});
-
-	/** For Import Modal **/
-	$("#import").click(function() 
+	$( "#search" ).keyup(function() 
 	{
-		$(".import-modal > .modal").css("display", "inline");
-		$('.import-modal').modal();
+		var search = $( this ).val();
+		ajax.search = search;
+		showList();
 	});
 
-	$("#importForm #btnImport").click(function() 
-	{
-		var formData =	new FormData();
-		formData.append('file',$('#import_csv')[0].files[0]);
-		ajax_call 	=	$.ajax({
-							url : '<?=MODULE_URL?>ajax/save_import',
-							data:	formData,
-							cache: 	false,
-							processData: false, 
-							contentType: false,
-							type: 	'POST',
-							success: function(response){
-								if(response && response.errmsg == ""){
-									$('#import-modal').modal('hide');
-									$(".alert-warning").addClass("hidden");
-									$("#errmsg").html('');
-									showList();
-								}else{
-									$('#import-modal').modal('hide');
-									show_error(response.errmsg);
-								}
-							},
-						});
-	});
-
-	$('#importForm').on('change', '#import_csv', function() {
-		var filename = $(this).val().split("\\");
-		$(this).closest('.input-group').find('.form-control').html(filename[filename.length - 1]);
-	});
-
-    $('#import-modal').on('show.bs.modal', function() {
-		var form_csv = $('#import_csv').val('').closest('.form-group').find('.form-control').html('').closest('.form-group').html();
-		$('#import_csv').closest('.form-group').html(form_csv);
-	});
-});
-
-function ajaxCallback(id) {
-	var ids = getDeleteId(id);
-	$.post('<?=BASE_URL?>maintenance/exchange_rate/ajax/delete', 'id=' + id, function(data) 
-	{
-		if( data.msg == 'success' )	
-		{
+	$('#pagination').on('click', 'a', function(e) {
+		e.preventDefault();
+		$('.checked').iCheck('uncheck');
+		var li = $(this).closest('li');
+		if (li.not('.active').length && li.not('.disabled').length) {
+			ajax.page = $(this).attr('data-page');
 			showList();
-			$(".alert-warning").addClass("hidden");
 		}
-		else
+	});
+
+	$(document).ready(function() 
+	{
+		showList();
+
+		$( "#exchangerate_table" ).on('click' , '.delete', function() 
 		{
+			var id = $( this ).attr("data-id");
+
+			if( id != "" )
+			{
+				$(".delete-modal > .modal").css("display", "inline");
+				$(".delete-modal").modal("show");
+
+				$( "#delete-yes" ).click(function() 
+				{
+					$.post('<?=BASE_URL?>maintenance/exchange_rate/ajax/delete', 'id=' + id, function(data) 
+					{
+						if( data.msg == 'success' )	
+						{
+							$(".delete-modal").modal("hide");
+							showList();
+						}
+						else
+						{			
+							$(".delete-modal").modal("hide");
+							show_error("Unable to delete the Exchange Rate(s).");
+						}
+					});
+				});	
+			}
+
+		});
+
+		/** For Import Modal **/
+		$("#import").click(function() 
+		{
+			$(".import-modal > .modal").css("display", "inline");
+			$('.import-modal').modal();
+		});
+
+		$("#importForm #btnImport").click(function() 
+		{
+			var formData =	new FormData();
+			formData.append('file',$('#import_csv')[0].files[0]);
+			ajax_call 	=	$.ajax({
+				url : '<?=MODULE_URL?>ajax/save_import',
+				data:	formData,
+				cache: 	false,
+				processData: false, 
+				contentType: false,
+				type: 	'POST',
+				success: function(response){
+					if(response && response.errmsg == ""){
+						$('#import-modal').modal('hide');
+						$(".alert-warning").addClass("hidden");
+						$("#errmsg").html('');
+						showList();
+					}else{
+						$('#import-modal').modal('hide');
+						show_error(response.errmsg);
+					}
+				},
+			});
+		});
+
+		$('#importForm').on('change', '#import_csv', function() {
+			var filename = $(this).val().split("\\");
+			$(this).closest('.input-group').find('.form-control').html(filename[filename.length - 1]);
+		});
+
+		$('#import-modal').on('show.bs.modal', function() {
+			var form_csv = $('#import_csv').val('').closest('.form-group').find('.form-control').html('').closest('.form-group').html();
+			$('#import_csv').closest('.form-group').html(form_csv);
+		});
+	});
+
+	function ajaxCallback(id) {
+		var ids = getDeleteId(id);
+		$.post('<?=BASE_URL?>maintenance/exchange_rate/ajax/delete', 'id=' + id, function(data) 
+		{
+			if( data.msg == 'success' )	
+			{
+				showList();
+				$(".alert-warning").addClass("hidden");
+			}
+			else
+			{
 			// Call function to display error_get_last
 			show_error(data.msg);
 		}
 	});
-}
+	}
 
-$(function() {
-	linkButtonToTable('#item_multiple_delete', '#exchangerate_table');
+	$(function() {
+		linkButtonToTable('#item_multiple_delete', '#exchangerate_table');
 	// linkButtonToTable('#activateMultipleBtn', '#exchangerate_table');
 	// linkButtonToTable('#deactivateMultipleBtn', '#exchangerate_table');
 	linkDeleteToModal('#exchangerate_table .delete', 'ajaxCallback');
@@ -262,9 +262,9 @@ $(function() {
 
 // Sorting Script
 tableSort('#exchangerate_table', function(value) {
-  ajax.sort = value;
-  ajax.page = 1;
-  showList();
+	ajax.sort = value;
+	ajax.page = 1;
+	showList();
 });
 
 
@@ -277,39 +277,40 @@ $('#items').on('change', function(){
 });
 
 
-		$('#exchangerate_table').on('click', '.activate', function() { 
-				var id = $(this).attr('data-id');
-				$.post('<?=MODULE_URL?>ajax/ajax_edit_activate', '&id='+id ,function(data) {
-					showList();
-				});
-			});
+var activate_id = '';
+$('#exchangerate_table').on('click', '.activate', function() { 
+	activate_id= $(this).attr('data-id');
+	$.post('<?=MODULE_URL?>ajax/ajax_edit_activate', '&id='+activate_id ,function(data) {
+		showList();
+	});
+});
+var deactivate_id = '';
+$('#exchangerate_table').on('click', '.deactivate', function() { 
+	$('#deactivate_modal').modal('show');
+	deactivate_id = $(this).attr('data-id');
 
-		$('#exchangerate_table').on('click', '.deactivate', function() { 
-			$('#deactivate_modal').modal('show');
-			var id = $(this).attr('data-id');
-			
-			$('#deactivate_modal').on('click', '#deactyes', function() {
-				$('#deactivate_modal').modal('hide');
-				
-				$.post('<?=MODULE_URL?>ajax/ajax_edit_deactivate', '&id='+id ,function(data) {
-					showList();
-				});
-			});
+	$('#deactivate_modal').on('click', '#deactyes', function() {
+		$('#deactivate_modal').modal('hide');
+
+		$.post('<?=MODULE_URL?>ajax/ajax_edit_deactivate', '&id='+deactivate_id ,function(data) {
+			showList();
 		});
+	});
+});
 
-	$("#deactivateMultipleBtn").click(function() 
-	{
+$("#deactivateMultipleBtn").click(function() 
+{
 	var id = [];
 
-		$('input:checkbox.item_checkbox:checked').each(function()
-		{
-			id.push($(this).val());
-		});
-		
-		if( id != "" )
-		{
-			$('#multipleDeactivateModal').modal('show');
-			$( "#multipleDeactivateModal #btnDeac" ).click(function() {
+	$('input:checkbox.item_checkbox:checked').each(function()
+	{
+		id.push($(this).val());
+	});
+
+	if( id != "" )
+	{
+		$('#multipleDeactivateModal').modal('show');
+		$( "#multipleDeactivateModal #btnDeac" ).click(function() {
 			ids 	=	getSelectedIds();
 			$.post('<?=MODULE_URL?>ajax/update_multiple_deactivate', "&ids="+ids ,function(data) {
 				
@@ -321,22 +322,22 @@ $('#items').on('change', function(){
 				} 
 			});
 		});
-		}
+	}
+});
+
+$("#activateMultipleBtn").click(function() 
+{
+	var id = [];
+
+	$('input:checkbox.item_checkbox:checked').each(function()
+	{
+		id.push($(this).val());
 	});
 
-	$("#activateMultipleBtn").click(function() 
+	if( id != "" )
 	{
-		var id = [];
-
-		$('input:checkbox.item_checkbox:checked').each(function()
-		{
-			id.push($(this).val());
-		});
-
-		if( id != "" )
-		{
-			$('#multipleActivateModal').modal('show');
-			$( "#multipleActivateModal #btnYes" ).click(function() {
+		$('#multipleActivateModal').modal('show');
+		$( "#multipleActivateModal #btnYes" ).click(function() {
 			ids 	=	getSelectedIds();
 			$.post('<?=MODULE_URL?>ajax/update_multiple_activate', "&ids="+ids ,function(data) {
 				if( data.msg == 'success' )
@@ -347,27 +348,27 @@ $('#items').on('change', function(){
 				} 
 			});
 		});
-		}
-	});
-
-	function getSelectedIds(){
-		id 	=	[];
-		$('.checkbox:checked').each(function(){
-			id.push($(this).val());
-		});
-		return id;
 	}
+});
 
-	$('#exchangerate_table').on('ifToggled', 'input[type=checkbox]:not(.checkall)', function() {
-			var b = $('input[type=checkbox]:not(.checkall)');
-			var row = $('#exchangerate_table >tbody >tr').length;
-			var c =	b.filter(':checked').length;
-			if(c == row){
-				$('#exchangerate_table thead tr th').find('.checkall').prop('checked', true).iCheck('update');
-			}else{
-				$('#exchangerate_table thead tr th').find('.checkall').prop('checked', false).iCheck('update');
-			}
-		});
+function getSelectedIds(){
+	id 	=	[];
+	$('.checkbox:checked').each(function(){
+		id.push($(this).val());
+	});
+	return id;
+}
+
+$('#exchangerate_table').on('ifToggled', 'input[type=checkbox]:not(.checkall)', function() {
+	var b = $('input[type=checkbox]:not(.checkall)');
+	var row = $('#exchangerate_table >tbody >tr').length;
+	var c =	b.filter(':checked').length;
+	if(c == row){
+		$('#exchangerate_table thead tr th').find('.checkall').prop('checked', true).iCheck('update');
+	}else{
+		$('#exchangerate_table thead tr th').find('.checkall').prop('checked', false).iCheck('update');
+	}
+});
 
 function historyOfMyLife() {
 	var arr = [];
