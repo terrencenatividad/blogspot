e0edfa87b87743ee48131fd1c228cc5ec8b62513
terensnatividad <terrence.natividad@cid-systems.com>
2019-01-18 16:31:05 +0800

    Price :

--- apanel/modules/inventory_module/backend/view/pricelist/pricelist_list.php
+++ apanel/modules/inventory_module/backend/view/pricelist/pricelist_list.php
@@ -17,10 +17,10 @@
 							</div>
 						</form> -->
 						<?= 
-							$ui->CreateNewButton('');
+						$ui->CreateNewButton('');
 						?>
 						<?= 
-							$ui->OptionButton('');
+						$ui->OptionButton('');
 						?>
 						<?=	$ui->CreateDeleteButton(''); ?>
 						<a class="btn btn-info btn-flat" role="button" href="<?=MODULE_URL?>master" style="outline:none;">Master Price List</a>
@@ -70,20 +70,20 @@
 						<table id="pricelist_table" class="table table-striped table-condensed table-bordered">
 							<thead>
 								<?php
-									echo $ui->loadElement('table')
-											->setHeaderClass('info')
-											->addHeader(
-												'<input type="checkbox" class="checkall">',
-												array(
-													'class' => 'text-center',
-													'style' => 'width: 100px'
-												)
-											)
-											->addHeader('Price List Code',array('class'=>'col-md-3'),'sort','pl.itemPriceCode')
-											->addHeader('Price List Name', array('class'=>'col-md-3'),'sort','pl.itemPriceName')
-											->addHeader('Description',array('class'=>'col-md-3'),'sort','pl.itemPriceDesc')
-											->addHeader('Status',array('class'=>'col-md-3'),'sort','pl.status')
-											->draw();
+								echo $ui->loadElement('table')
+								->setHeaderClass('info')
+								->addHeader(
+									'<input type="checkbox" class="checkall">',
+									array(
+										'class' => 'text-center',
+										'style' => 'width: 100px'
+									)
+								)
+								->addHeader('Price List Code',array('class'=>'col-md-3'),'sort','pl.itemPriceCode')
+								->addHeader('Price List Name', array('class'=>'col-md-3'),'sort','pl.itemPriceName')
+								->addHeader('Description',array('class'=>'col-md-3'),'sort','pl.itemPriceDesc')
+								->addHeader('Status',array('class'=>'col-md-3'),'sort','pl.status')
+								->draw();
 								?>
 							</thead>
 							<tbody id="list_container"></tbody>
@@ -105,116 +105,116 @@
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">×</span></button>
-						<h4 class="modal-title">Import Price List</h4>
-					</div>
-					<div class="modal-body">
-						<label>Step 1. Download the sample template <a href="<?=MODULE_URL?>get_import" download="PriceList_Template.csv">here</a></label>
-						<hr/>
-						<label>Step 2. Fill up the information needed for each columns of the template.</label>
-						<hr/>
-						<div class="form-group">
-							<label for="import_csv">Step 3. Select the updated file and click 'Import' to proceed.</label>
-							<?php
-								echo $ui->setElement('file')
-										->setId('import_csv')
-										->setName('import_csv')
-										->setAttribute(array('accept' => '.csv'))
-										->setValidation('required')
-										->draw();
-							?>
-							<span class="help-block"></span>
+							<h4 class="modal-title">Import Price List</h4>
 						</div>
-						<p class="help-block">The file to be imported must be in CSV (Comma Separated Values) file.</p>
-					</div>
-					<div class="modal-footer text-center">
-						<button type="button" class="btn btn-info btn-flat" id = "btnImport">Import</button>
-						<button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Close</button>
-					</div>
-				</form>
+						<div class="modal-body">
+							<label>Step 1. Download the sample template <a href="<?=MODULE_URL?>get_import" download="PriceList_Template.csv">here</a></label>
+							<hr/>
+							<label>Step 2. Fill up the information needed for each columns of the template.</label>
+							<hr/>
+							<div class="form-group">
+								<label for="import_csv">Step 3. Select the updated file and click 'Import' to proceed.</label>
+								<?php
+								echo $ui->setElement('file')
+								->setId('import_csv')
+								->setName('import_csv')
+								->setAttribute(array('accept' => '.csv'))
+								->setValidation('required')
+								->draw();
+								?>
+								<span class="help-block"></span>
+							</div>
+							<p class="help-block">The file to be imported must be in CSV (Comma Separated Values) file.</p>
+						</div>
+						<div class="modal-footer text-center">
+							<button type="button" class="btn btn-info btn-flat" id = "btnImport">Import</button>
+							<button type="button" class="btn btn-default btn-flat" data-dismiss="modal">Close</button>
+						</div>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<script>
-var ajax = filterFromURL();
-var ajax_call = '';
+	<script>
+		var ajax = filterFromURL();
+		var ajax_call = '';
 
-$('#table_search').on('input', function () {
-	ajax.page = 1;
-	ajax.search = $(this).val();
-	showList();
-});
+		$('#table_search').on('input', function () {
+			ajax.page = 1;
+			ajax.search = $(this).val();
+			showList();
+		});
 
-$('#pagination').on('click', 'a', function(e) {
-	e.preventDefault();
-	$('.checked').iCheck('uncheck');
-	var li = $(this).closest('li');
-	if (li.not('.active').length && li.not('.disabled').length) {
-		ajax.page = $(this).attr('data-page');
-		showList();
-	}
-});
+		$('#pagination').on('click', 'a', function(e) {
+			e.preventDefault();
+			$('.checked').iCheck('uncheck');
+			var li = $(this).closest('li');
+			if (li.not('.active').length && li.not('.disabled').length) {
+				ajax.page = $(this).attr('data-page');
+				showList();
+			}
+		});
 
-$('#items').on('change', function(){
-	ajax.page = 1;
-	ajax.limit = $(this).val();
-	showList();
-});
+		$('#items').on('change', function(){
+			ajax.page = 1;
+			ajax.limit = $(this).val();
+			showList();
+		});
 
-ajaxToFilter(ajax,{ search: '#table_search',  limit: '#items'});
+		ajaxToFilter(ajax,{ search: '#table_search',  limit: '#items'});
 
-function show_error(msg)
-{
-	$(".delete-modal").modal("hide");
-	$(".alert-warning").removeClass("hidden");
-	$("#errmsg").html(msg);
-}
-
-function show_success_msg(msg)
-{
-	$('#success_modal #message').html(msg);
-	$('#success_modal').modal('show');
-	setTimeout(function() {												
-		window.location = '<?= MODULE_URL ?>';		
-	}, 1000)
-}
-
-function showList(pg){
-	filterToURL();
-	if (ajax_call != '') {
-		ajax_call.abort();
-	}
-	ajax_call 	=	$.post('<?=BASE_URL?>maintenance/pricelist/ajax/price_list',ajax, function(data) {
-						$('#pricelist_table #list_container').html(data.table);
-						$('#pagination').html(data.pagination);
-						historyOfMyLife();
-						$("#export_id").attr('href', 'data:text/csv;filename=testing.csv;charset=utf-8,' + encodeURIComponent(data.csv));
-						if (ajax.page > data.page_limit && data.page_limit > 0) {
-							ajax.page = data.page_limit;
-							showList();
-						}
-					});
-};
-
-function ajaxCallback(id) {
-	var ids = getDeleteId(id);
-	$.post('<?=MODULE_URL?>ajax/ajax_delete', ids, function(data) {
-		if ( ! data.success) {
-			$('#warning_modal #warning_message').html('<p>Unable to delete Pricelist: Template in Use</p>');
-			data.error_id.forEach(function(id) {
-				$('#warning_modal #warning_message').append('<p>Pricelist Code: ' + id + '</p>');
-			});
-			$('#warning_modal').modal('show');
+		function show_error(msg)
+		{
+			$(".delete-modal").modal("hide");
+			$(".alert-warning").removeClass("hidden");
+			$("#errmsg").html(msg);
 		}
-		showList();
-	});
-}
-$(function() {
-	linkButtonToTable('#item_multiple_delete', '#pricelist_table');
-	linkDeleteToModal('#pricelist_table .delete', 'ajaxCallback');
-	linkDeleteMultipleToModal('#item_multiple_delete', '#pricelist_table', 'ajaxCallback');
-});
+
+		function show_success_msg(msg)
+		{
+			$('#success_modal #message').html(msg);
+			$('#success_modal').modal('show');
+			setTimeout(function() {												
+				window.location = '<?= MODULE_URL ?>';		
+			}, 1000)
+		}
+
+		function showList(pg){
+			filterToURL();
+			if (ajax_call != '') {
+				ajax_call.abort();
+			}
+			ajax_call 	=	$.post('<?=BASE_URL?>maintenance/pricelist/ajax/price_list',ajax, function(data) {
+				$('#pricelist_table #list_container').html(data.table);
+				$('#pagination').html(data.pagination);
+				historyOfMyLife();
+				$("#export_id").attr('href', 'data:text/csv;filename=testing.csv;charset=utf-8,' + encodeURIComponent(data.csv));
+				if (ajax.page > data.page_limit && data.page_limit > 0) {
+					ajax.page = data.page_limit;
+					showList();
+				}
+			});
+		};
+
+		function ajaxCallback(id) {
+			var ids = getDeleteId(id);
+			$.post('<?=MODULE_URL?>ajax/ajax_delete', ids, function(data) {
+				if ( ! data.success) {
+					$('#warning_modal #warning_message').html('<p>Unable to delete Pricelist: Template in Use</p>');
+					data.error_id.forEach(function(id) {
+						$('#warning_modal #warning_message').append('<p>Pricelist Code: ' + id + '</p>');
+					});
+					$('#warning_modal').modal('show');
+				}
+				showList();
+			});
+		}
+		$(function() {
+			linkButtonToTable('#item_multiple_delete', '#pricelist_table');
+			linkDeleteToModal('#pricelist_table .delete', 'ajaxCallback');
+			linkDeleteMultipleToModal('#item_multiple_delete', '#pricelist_table', 'ajaxCallback');
+		});
 
 
 // function ajaxCallback(id) {
@@ -232,7 +232,7 @@ $(function() {
 // 	return "id[]=" + x.join("&id[]=");
 // }
 
-	$(function() {
+$(function() {
 			// linkButtonToTable('#activateMultipleBtn', '#pricelist_table');
 			// linkButtonToTable('#deactivateMultipleBtn', '#pricelist_table');
 		});
@@ -244,7 +244,7 @@ $(document).ready(function()
 	// $( "#pricelist_table" ).on('click' , '.delete', function() 
 	// {
 	// 	var id = $( this ).attr("data-id");
-		
+	
 	// 	if( id != "" )
 	// 	{
 	// 		$(".delete-modal > .modal").css("display", "inline");
@@ -304,42 +304,42 @@ $(document).ready(function()
 
 	/** For Import Modal **/
 	$("#import").click(function() 
-    {
-        $(".import-modal > .modal").css("display", "inline");
-        $('.import-modal').modal();
-    });
-    
-    $("#importForm #btnImport").click(function() 
-    {
-        var formData =	new FormData();
-        formData.append('file',$('#import_csv')[0].files[0]);
-        ajax_call 	=	$.ajax({
-                            url : '<?=MODULE_URL?>ajax/save_import',
-                            data:	formData,
-                            cache: 	false,
-                            processData: false, 
-                            contentType: false,
-                            type: 	'POST',
-                            success: function(response){
-                                if(response && response.errmsg == ""){
-                                    $('#import-modal').modal('hide');
-                                    $(".alert-warning").addClass("hidden");
-                                    $("#errmsg").html('');
-                                    show_success_msg('Your Data has been imported successfully.');
-                                }else{
-                                    $('#import-modal').modal('hide');
-                                    show_error(response.errmsg);
-                                }
-                            },
-                        });
-    });
+	{
+		$(".import-modal > .modal").css("display", "inline");
+		$('.import-modal').modal();
+	});
+	
+	$("#importForm #btnImport").click(function() 
+	{
+		var formData =	new FormData();
+		formData.append('file',$('#import_csv')[0].files[0]);
+		ajax_call 	=	$.ajax({
+			url : '<?=MODULE_URL?>ajax/save_import',
+			data:	formData,
+			cache: 	false,
+			processData: false, 
+			contentType: false,
+			type: 	'POST',
+			success: function(response){
+				if(response && response.errmsg == ""){
+					$('#import-modal').modal('hide');
+					$(".alert-warning").addClass("hidden");
+					$("#errmsg").html('');
+					show_success_msg('Your Data has been imported successfully.');
+				}else{
+					$('#import-modal').modal('hide');
+					show_error(response.errmsg);
+				}
+			},
+		});
+	});
 
-    $('#importForm').on('change', '#import_csv', function() {
+	$('#importForm').on('change', '#import_csv', function() {
 		var filename = $(this).val().split("\\");
 		$(this).closest('.input-group').find('.form-control').html(filename[filename.length - 1]);
 	});
 
-    $('#import-modal').on('show.bs.modal', function() {
+	$('#import-modal').on('show.bs.modal', function() {
 		var form_csv = $('#import_csv').val('').closest('.form-group').find('.form-control').html('').closest('.form-group').html();
 		$('#import_csv').closest('.form-group').html(form_csv);
 	});
@@ -372,7 +372,7 @@ tableSort('#pricelist_table', function(value, getlist) {
 	}
 },ajax);
 
-		$('#export_id').prop('download','price_list.csv');
+$('#export_id').prop('download','price_list.csv');
 		// $('#export_id').prop('href','<?= MODULE_URL ?>get_export');
 		$('#import_id').prop('href','#import-modal');
 		// $('#import_id').prop('data-toggle','modal');
@@ -381,79 +381,79 @@ tableSort('#pricelist_table', function(value, getlist) {
 			$("#import-modal > .modal").css("display", "inline");
 			$('#import-modal').modal();
 		});
-
+		var activate_id = '';
 		$('#pricelist_table').on('click', '.activate', function() { 
-			var id = $(this).attr('data-id');
-			$.post('<?=MODULE_URL?>ajax/ajax_edit_activate', '&id='+id ,function(data) {
+			activate_id = $(this).attr('data-id');
+			$.post('<?=MODULE_URL?>ajax/ajax_edit_activate', '&id='+activate_id ,function(data) {
 				showList();
 			});
 		});
-
+		var deactivate_id = '';
 		$('#pricelist_table').on('click', '.deactivate', function() { 
 			$('#deactivate_modal').modal('show');
-			var id = $(this).attr('data-id');
+			deactivate_id = $(this).attr('data-id');
 			
 			$('#deactivate_modal').on('click', '#deactyes', function() {
 				$('#deactivate_modal').modal('hide');
 				
-				$.post('<?=MODULE_URL?>ajax/ajax_edit_deactivate', '&id='+id ,function(data) {
+				$.post('<?=MODULE_URL?>ajax/ajax_edit_deactivate', '&id='+deactivate_id ,function(data) {
 					showList();
 				});
 			});
 		});
 
 		$("#deactivateMultipleBtn").click(function() 
-	{
-	var id = [];
+		{
+			var id = [];
 
-		$('input:checkbox.item_checkbox:checked').each(function()
-		{
-			id.push($(this).val());
-		});
-		
-		if( id != "" )
-		{
-			$('#multipleDeactivateModal').modal('show');
-			$( "#multipleDeactivateModal #btnDeac" ).click(function() {
-			ids 	=	getSelectedIds();
-			$.post('<?=MODULE_URL?>ajax/update_multiple_deactivate', "&ids="+ids ,function(data) {
-				
-				if( data.msg == 'success' )
-				{
-					$('.checked').iCheck('uncheck');
-					showList();
-					$('#multipleDeactivateModal').modal('hide');
-				} 
+			$('input:checkbox.item_checkbox:checked').each(function()
+			{
+				id.push($(this).val());
 			});
+			
+			if( id != "" )
+			{
+				$('#multipleDeactivateModal').modal('show');
+				$( "#multipleDeactivateModal #btnDeac" ).click(function() {
+					ids 	=	getSelectedIds();
+					$.post('<?=MODULE_URL?>ajax/update_multiple_deactivate', "&ids="+ids ,function(data) {
+						
+						if( data.msg == 'success' )
+						{
+							$('.checked').iCheck('uncheck');
+							showList();
+							$('#multipleDeactivateModal').modal('hide');
+						} 
+					});
+				});
+			}
 		});
-		}
-	});
 
-	$("#activateMultipleBtn").click(function() 
-	{
-		var id = [];
-
-		$('input:checkbox.item_checkbox:checked').each(function()
+		$("#activateMultipleBtn").click(function() 
 		{
-			id.push($(this).val());
-		});
+			var id = [];
 
-		if( id != "" )
-		{
-			$('#multipleActivateModal').modal('show');
-			$( "#multipleActivateModal #btnYes" ).click(function() {
-			ids 	=	getSelectedIds();
-			$.post('<?=MODULE_URL?>ajax/update_multiple_activate', "&ids="+ids ,function(data) {
-				if( data.msg == 'success' )
-				{
-					$('.checked').iCheck('uncheck');
-					showList();
-					$('#multipleActivateModal').modal('hide');
-				} 
+			$('input:checkbox.item_checkbox:checked').each(function()
+			{
+				id.push($(this).val());
 			});
+
+			if( id != "" )
+			{
+				$('#multipleActivateModal').modal('show');
+				$( "#multipleActivateModal #btnYes" ).click(function() {
+					ids 	=	getSelectedIds();
+					$.post('<?=MODULE_URL?>ajax/update_multiple_activate', "&ids="+ids ,function(data) {
+						if( data.msg == 'success' )
+						{
+							$('.checked').iCheck('uncheck');
+							showList();
+							$('#multipleActivateModal').modal('hide');
+						} 
+					});
+				});
+			}
 		});
-		}
-	});
 
 		function getSelectedIds(){
 			id 	=	[];
@@ -474,20 +474,20 @@ tableSort('#pricelist_table', function(value, getlist) {
 			}
 		});
 
-function historyOfMyLife() {
-	var arr = [];
-	$('#pricelist_table tbody').find('.label').each(function(index, value){
-		arr.push($(this).html());
-		if(jQuery.inArray('ACTIVE', arr) != -1) {
-			$('#deactivateMultipleBtn').attr('disabled', false);
-		}else{
-			$('#deactivateMultipleBtn').attr('disabled', true);
+		function historyOfMyLife() {
+			var arr = [];
+			$('#pricelist_table tbody').find('.label').each(function(index, value){
+				arr.push($(this).html());
+				if(jQuery.inArray('ACTIVE', arr) != -1) {
+					$('#deactivateMultipleBtn').attr('disabled', false);
+				}else{
+					$('#deactivateMultipleBtn').attr('disabled', true);
+				}
+				if(jQuery.inArray('INACTIVE', arr) != -1) {
+					$('#activateMultipleBtn').attr('disabled', false);
+				}else{
+					$('#activateMultipleBtn').attr('disabled', true);
+				}
+			});
 		}
-		if(jQuery.inArray('INACTIVE', arr) != -1) {
-			$('#activateMultipleBtn').attr('disabled', false);
-		}else{
-			$('#activateMultipleBtn').attr('disabled', true);
-		}
-	});
-}
-</script>
+	</script>
