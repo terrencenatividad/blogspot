22c4b0c65816d66f56305cdbe73cf1dadfdbab87
Jasmine Ang <jasmine.ang@cid-systems.com>
2019-01-28 13:38:04 +0800

    AP : Added parenthesis on credit amount under currency column

--- apanel/modules/financials_module/backend/view/accounts_payable/accounts_payable_view.php
+++ apanel/modules/financials_module/backend/view/accounts_payable/accounts_payable_view.php
@@ -263,7 +263,7 @@
                                             <?php if($row->debit == 0)  { ?>
                                                 <td class = "remove-margin">
                                                     <div class="col-md-12 currencyamount text-right">
-                                                        <?php echo number_format($row->convertedcredit, 2);?>
+                                                        <?php echo '('. number_format($row->convertedcredit, 2) .')';?>
                                                     </div>
                                                 </td>
                                             <?php } else { ?>
