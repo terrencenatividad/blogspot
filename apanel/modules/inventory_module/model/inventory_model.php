2c8bf4985fd9d224b75e671ab6b6565fc962fce0
Marion Marion <marion.rosales@cid-systems.com>
2019-01-31 15:31:43 +0800

    Issue Parts: Fix issued quantity

--- apanel/modules/inventory_module/model/inventory_model.php
+++ apanel/modules/inventory_module/model/inventory_model.php
@@ -150,11 +150,11 @@ class inventory_model extends wc_model {
 
 		$inv_check = $this->db->setTable("($inner_query) i")
 								->leftJoin('invdtlfile id ON i.ic = id.itemcode AND i.companycode = id.companycode AND i.wh = id.warehouse')
-								->setFields('i.ic itemcode, i.wh warehouse, beginningQty, IFNULL(bb, 0) - IFNULL(beginningQty, 0) bb, salesorderQty, IFNULL(so, 0) - IFNULL(salesorderQty, 0) so, deliveredQty,  IFNULL(dr, 0) - IFNULL(deliveredQty, 0) dr, salesinvoiceQty, IFNULL(si, 0) - IFNULL(salesinvoiceQty, 0) si, salesreturnQty, IFNULL(sr, 0) - IFNULL(salesreturnQty, 0) sr, salesscrapQty, IFNULL(xr, 0) - IFNULL(salesscrapQty, 0) xr, purchaseorderQty, IFNULL(po, 0) - IFNULL(purchaseorderQty, 0) po, purchasereceiptQty,  IFNULL(pr, 0) - IFNULL(purchasereceiptQty, 0) pr, purchasereturnQty, IFNULL(pt, 0) - IFNULL(purchasereturnQty, 0) pt, adjustmentsQty, IFNULL(ia, 0) - IFNULL(adjustmentsQty, 0) ia, transferedQty, IFNULL(st, 0) - IFNULL(transferedQty, 0) st, IFNULL(jo, 0) - IFNULL(joborderQty, 0) jo, IFNULL(jr, 0) - IFNULL(jobreleasedQty, 0) jr')
+								->setFields('i.ic itemcode, i.wh warehouse, beginningQty, IFNULL(bb, 0) - IFNULL(beginningQty, 0) bb, salesorderQty, IFNULL(so, 0) - IFNULL(salesorderQty, 0) so, deliveredQty,  IFNULL(dr, 0) - IFNULL(deliveredQty, 0) dr, salesinvoiceQty, IFNULL(si, 0) - IFNULL(salesinvoiceQty, 0) si, salesreturnQty, IFNULL(sr, 0) - IFNULL(salesreturnQty, 0) sr, salesscrapQty, IFNULL(xr, 0) - IFNULL(salesscrapQty, 0) xr, purchaseorderQty, IFNULL(po, 0) - IFNULL(purchaseorderQty, 0) po, purchasereceiptQty,  IFNULL(pr, 0) - IFNULL(purchasereceiptQty, 0) pr, purchasereturnQty, IFNULL(pt, 0) - IFNULL(purchasereturnQty, 0) pt, adjustmentsQty, IFNULL(ia, 0) - IFNULL(adjustmentsQty, 0) ia, transferedQty, IFNULL(st, 0) - IFNULL(transferedQty, 0) st, joborderQty, IFNULL(jo, 0) - IFNULL(joborderQty, 0) jo, jobreleasedQty, IFNULL(jr, 0) - IFNULL(jobreleasedQty, 0) jr')
 								->setHaving('bb != 0 OR so != 0 OR dr != 0 OR si != 0 OR sr != 0 OR xr != 0 OR po != 0 OR pr != 0 OR pt != 0 OR ia != 0 OR st != 0 OR jo != 0 OR jr != 0')
 								->runSelect()
 								->getResult();
-
+								
 		if (empty($inv_check)) {
 			$check_transactions = $this->db->setTable("($inner_query) i")
 											->setFields('*')
@@ -177,13 +177,14 @@ class inventory_model extends wc_model {
 			$result = $this->db->setTable('invdtlfile')
 							->setWhere('companycode IS NOT NULL')
 							->runDelete();
+
 							
 			if ($result) {
 				$result = $this->db->setTable('invdtlfile')
 									->setFields($fields)
 									->setInsertSelect($inner_query)
 									->runInsert(false);
-				
+									
 				if ($result) {
 					$result = $this->db->setTable('invfile')
 								->setWhere('companycode IS NOT NULL')
