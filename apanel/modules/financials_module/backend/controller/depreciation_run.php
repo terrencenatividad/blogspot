b0ba7c688358c4c80e46e62d8d16402be7abdef0
Marion Marion <marion.rosales@cid-systems.com>
2019-02-01 10:32:21 +0800

    Depreciation Run

--- apanel/modules/financials_module/backend/controller/depreciation_run.php
+++ apanel/modules/financials_module/backend/controller/depreciation_run.php
@@ -35,51 +35,28 @@ class controller extends wc_controller {
 	}
 
 	private function ajax_list() {
-		$data		= $this->input->post(array('search', 'sort','checked'));
-		$sort		= $data['sort'];
-		$search		= $data['search'];
-		$checked	= $data['checked'];
-		$pagination	= $this->depreciation_run->getAsset($this->fields);
-		
+		$sort = $this->input->post('sort');
+		$pagination		= $this->depreciation_run->getAssetMasterList($this->fields,$sort);
+
 		$table		= '';
 		if (empty($pagination)) {
-			$table = '<tr><td colspan="2" class="text-center"><b>No Records Found</b></td></tr>';
+			$table = '<tr><td colspan="9" class="text-center"><b>No Records Found</b></td></tr>';
 		}
-
 		foreach ($pagination as $key => $row) {
-			$table .= '<tr class="info">';
-			$table .= '<td class = "col-md-2"><b>Asset Class</b></td><td colspan = "5">'.$row->assetclass.'</td>';
-			$table .= '</tr>'; 
-			
-			$asset = $this->depreciation_run->getAssetDetails($row->id);
-			foreach ($asset as $row) {
-				$table .= '<tr class="success"><td class = "col-md-2"><b>' . 'Asset Number' . '</b></td><td colspan = "5">' . $row->asset_number . '</td></tr>';
-				$table .= '<tr class="warning"><td class = "col-md-2"><b>' . 'Budget Center' . '</b></td><td colspan = "5">' . $row->name . '</td></tr>';
-				$table .= '<tr class="warning"><td class = "col-md-2"><b>' . 'Capitalized Cost' . '</b></td><td colspan = "5">' . number_format($row->capitalized_cost, 2) . '</td></tr>';
-				$table .= '<tr class="warning"><td class = "col-md-2"><b>' . 'Depreciation / month' . '</b></td><td colspan = "5">' . number_format(($row->balance_value - $row->salvage_value) / $row->useful_life, 2) . '</td></tr>';
-				
-				$comp = $this->depreciation_run->getDepreciationDetails($row->asset_number);
-				if (!empty($comp)) {
-					$table .= '<tr class="warning">';
-					$table .= '<th class="text-left">' . 'Date' . '</td>';
-					$table .= '<th class="text-left">' . 'Depreciation Amount' . '</td>';
-					$table .= '<th class="text-left">' . 'Accumulated Depreciation Amount' . '</td>';
-					$table .= '<th class="text-left">' . 'GL Account(Asset)' . '</td>';
-					$table .= '<th class="text-left">' . 'GL Account(AccDep)' . '</td>';
-					$table .= '<th class="text-left">' . 'GL Account(DepExp)' . '</td>';
-					$table .= '</tr>';
-				}
-				foreach ($comp as $row) {
-					$table .= '<tr>';
-					$table .= '<td class="text-left">'.date("M d, Y", strtotime($row->depreciation_date)).'</td>';
-					$table .= '<td class="text-right">'.number_format($row->depreciation_amount, 2).'</td>';
-					$table .= '<td class="text-right">'.number_format($row->accumulated_dep, 2).'</td>';
-					$table .= '<td class="text-left">'.$row->a_segment5 . ' - '. $row->asset.'</td>';
-					$table .= '<td class="text-left">'.$row->b_segment5 . ' - '. $row->accdep.'</td>';
-					$table .= '<td class="text-left">'.$row->c_segment5 . ' - '. $row->depexp.'</td>';
-					$table .= '</tr>';
-				}
-			}
+				$table .= '<tr>';
+				$table .= '<td class="text-left">' . date('M d, Y', strtotime($row->depreciation_date)) .'</td>';
+				$table .= '<td class="text-left">' . $row->name . '</td>';
+				$table .= '<td class="text-left">' . $row->asset_number . '</td>';
+				$table .= '<td class="text-left">' . $row->serial_number . '</td>';
+				$table .= '<td class="text-left">' . $row->assetclass . '</td>';
+				$table .= '<td class="text-left">' . $row->description . '</td>';
+				$table .= '<td class="text-right">' . number_format($row->capitalized_cost, 2) . '</td>';
+				$table .= '<td class="text-right">' . number_format($row->depreciation_amount, 2) . '</td>';
+				$table .= '<td class="text-right">' . number_format($row->accumulated_dep, 2) . '</td>';
+				$table .= '<td>' . $row->asset . '</td>';
+				$table .= '<td>' . $row->accdep . '</td>';
+				$table .= '<td>' . $row->depexp . '</td>';
+				$table .= '</tr>';
 		}
 
 		return array('table' => $table);
@@ -95,47 +72,22 @@ class controller extends wc_controller {
 		if (empty($pagination)) {
 			$table = '<tr><td colspan="2" class="text-center"><b>No Records Found</b></td></tr>';
 		}
-		foreach ($pagination as $row) {
-			$table .= '<tr class="info">';
-			$dropdown = $this->ui->loadElement('check_task')
-								->addView()
-								->addEdit()
-								->draw();
-			$table .= '<td><b>' . 'Asset Class' . '</b></td>';
-			$table .= '<td colspan="5">' . $row->assetclass . '</td>';
-			$table .= '</tr>';
-
-			$table .= '<tr class="success"><td class="col-md-2"><b>' . 'Asset Number' . '</b></td><td colspan="5">' . $row->asset_number . '</td></tr>';
-				$table .= '<tr class="warning"><td class="col-md-2"><b>' . 'Budget Center' . '</b></td><td colspan="5">' . $row->name . '</td></tr>';
-				$table .= '<tr class="warning"><td class="col-md-2"><b>' . 'Capitalized Cost' . '</b></td><td colspan="5">' . number_format($row->capitalized_cost, 2) . '</td></tr>';
-				$table .= '<tr class="warning"><td class="col-md-2"><b>' . 'Depreciation / month' . '</b></td><td colspan="5">' . number_format(($row->balance_value - $row->salvage_value) / $row->useful_life, 2) . '</td></tr>';
-				
-			$table .= '<tr class="warning">';
-			$table .= '<th>' . 'Date' . '</td>';
-			$table .= '<th>' . 'Depreciation Amount' . '</td>';
-			$table .= '<th>' . 'Accumulated Depreciation Amount' . '</td>';
-			$table .= '<th>' . 'GL Account(Asset)' . '</td>';
-			$table .= '<th>' . 'GL Account(AccDep)' . '</td>';
-			$table .= '<th>' . 'GL Account(DepExp)' . '</td>';
-			$table .= '</tr>';
+		foreach ($pagination as $key => $row) {
 			$table .= '<tr>';
-			$depreciation = 0;  
-			$time  = strtotime($row->depreciation_month);
-			for($x=1;$x<=$row->useful_life;$x++){
-			$depreciation += ($row->balance_value - $row->salvage_value) / $row->useful_life;
-			$final = date("M d, Y", strtotime("+$x month", $time));
-			$table .= '<tr>';
-			$table .= '<td>' . $final . '</td>';
-			$table .= '<td>' . number_format(($row->balance_value - $row->salvage_value) / $row->useful_life, 2) . '</td>';
-			$table .= '<td>' . number_format($depreciation, 2) . '</td>';
-			$table .= '<td>' . $row->a_segment5 .' - '. $row->asset . '</td>';
-			$table .= '<td>' . $row->b_segment5 .' - '. $row->accdep . '</td>';
-			$table .= '<td>' . $row->c_segment5 .' - '. $row->depexp . '</td>';
+			$table .= '<td class="text-left">' . date('M d, Y', strtotime($row->depreciation_date)) .'</td>';
+			$table .= '<td class="text-left">' . $row->name . '</td>';
+			$table .= '<td class="text-left">' . $row->asset_number . '</td>';
+			$table .= '<td class="text-left">' . $row->serial_number . '</td>';
+			$table .= '<td class="text-left">' . $row->assetclass . '</td>';
+			$table .= '<td class="text-left">' . $row->description . '</td>';
+			$table .= '<td class="text-right">' . number_format($row->capitalized_cost, 2) . '</td>';
+			$table .= '<td class="text-right">' . number_format($row->depreciation_amount, 2) . '</td>';
+			$table .= '<td class="text-right">' . number_format($row->accumulated_dep, 2) . '</td>';
+			$table .= '<td>' . $row->asset . '</td>';
+			$table .= '<td>' . $row->accdep . '</td>';
+			$table .= '<td>' . $row->depexp . '</td>';
 			$table .= '</tr>';
-	
-
-			}
-		}
+	}
 
 			$table .= '</tr>';
 
