0c2af55421eac778f66f2b1d653904970065071d
Christopher Dalisay <christopher.dalisay@cid-systems.com>
2019-02-01 11:49:44 +0800

    SQ adjust uom column on print

--- apanel/modules/parts_and_service/backend/controller/service_quotation.php
+++ apanel/modules/parts_and_service/backend/controller/service_quotation.php
@@ -502,7 +502,7 @@ class controller extends wc_controller {
 				// ->addTermsAndCondition()
 				->addReceived();
 
-		$print->setHeaderWidth(array(25, 45, 20, 10, 10, 20, 20, 20, 30))
+		$print->setHeaderWidth(array(25, 40, 20, 10, 15, 20, 20, 20, 30))
 				->setHeaderAlign(array('C', 'C', 'C', 'C', 'C', 'C', 'C','C','C'))
 				->setHeader(array('Item Code', 'Description', 'Warranty', 'Qty', 'UOM', 'Price','Discount','Tax','Amount'))
 				->setRowAlign(array('L', 'L', 'L', 'R', 'L', 'R', 'R','R','R'))
