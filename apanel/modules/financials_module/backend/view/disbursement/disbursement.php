ccf02765a7c0e861fc87df9db7fad8d6b4878edd
terensnatividad <terrence.natividad@cid-systems.com>
2019-02-07 13:55:40 +0800

    DV : view

--- apanel/modules/financials_module/backend/view/disbursement/disbursement.php
+++ apanel/modules/financials_module/backend/view/disbursement/disbursement.php
@@ -1350,7 +1350,7 @@
 				$('#entriesTable tbody tr.clone select').select2('destroy');
 			}
 
-			val_bank = $(this).val();
+			val_bank = $('.cheque_account :selected').text();
 			$('#current_bank').val(val_bank);
 			var num = curr_bank_seq[val_bank] || 0;
 			cheque_element = $(this);
