082889fc94cce282c5057e7f1f08db4019ce4c05
Juliesse Rasco <juliesse.rasco@cid-systems.com>
2019-02-18 11:06:37 +0800

    SAWT: Updated Code for CSV

--- apanel/modules/reports_module/backend/controller/sawt.php
+++ apanel/modules/reports_module/backend/controller/sawt.php
@@ -94,32 +94,11 @@ class controller extends wc_controller {
         $table .= '<td colspan = "9" style = "text-align:right">' . number_format($totalwtaxamount, 2) . '</td>';
         $table .= '</tr>';
 
-		return array('table' => $table);
+		$csv 	= $this->sawt_csv($date[0], $month, $year);
+		return array('table' => $table, 'csv' => $csv);
 	}
 
-    public function sawt_csv() {
-		if (!empty($_GET["datepicker"])) {
-			$datefilter = $_GET['datepicker'];
-			$date = explode("-", $datefilter);
-			if($date[0] == 'January'){$month = '01';}
-			else if($date[0] == 'February'){$month = '02';}
-			else if($date[0] == 'March'){$month = '03';}
-			else if($date[0] == 'April'){$month = '04';}
-			else if($date[0] == 'May'){$month = '05';}
-			else if($date[0] == 'June'){$month = '06';}
-			else if($date[0] == 'July'){$month = '07';}
-			else if($date[0] == 'August'){$month = '08';}
-			else if($date[0] == 'September'){$month = '09';}
-			else if($date[0] == 'October'){$month = '10';}
-			else if($date[0] == 'November'){$month = '11';}
-			else if($date[0] == 'December'){$month = '12';}
-
-			$year = $date[1];
-		}
-		else {
-			$month = '';
-			$year = '';
-		}
+    public function sawt_csv($full_month, $month, $year) {
 
         $company =  $this->sawt_model->getCompanyDetails($this->fields);
 		$pagination	= $this->sawt_model->getSawtPagination($month, $year);
@@ -133,137 +112,56 @@ class controller extends wc_controller {
 			$form = '1702';
 		}
 
-		$excel = new PHPExcel();
-		$excel->getProperties()
-				->setCreator('Cid')
-				->setLastModifiedBy('Cid')
-				->setTitle($filename)
-				->setSubject('SAWT')
-				->setDescription('SAWT')
-				->setKeywords('SAWT')
-				->setCategory('SAWT');
+		$header 	=	array('SEQ NO', 'TAXPAYER IDENTIFICATION NO', 'CORPORATION (Registered Name)', 'INDIVIDUAL', 'ATC CODE', 'NATURE OF PAYMENT', 'AMOUNT OF INCOME PAYMENT', 'TAX RATE', 'AMOUNT OF TAX WITHHELD');
+		
+		$table = '';
+		$table .= '"BIR FORM '.$form.'"';
+		$table .= "\n";
+		$table .= '"SUMMARY ALPHALIST OF WITHHOLDING TAXES (SAWT)"';
+		$table .= "\n";
+		$table .= '"FOR THE MONTH OF '.strtoupper($full_month). ' '.$year.'"';
+		$table .= "\n\n";
+		$table .= '"TIN : ","'.$company->tin.'"';
+		$table .= "\n";
+		$table .= '"PAYEE\'S NAME : ","'.strtoupper($company->companyname).'"';
+		$table .= "\n\n";
 
-		$excel->getActiveSheet()->setTitle('SAWT');
-		$excel->setActiveSheetIndex(0);
-		$sheet = $excel->getActiveSheet();
-
-		$sheet->getCell('A1')->setValue('BIR FORM '.$form);
-        $sheet->getCell('A2')->setValue('SUMMARY ALPHALIST OF WITHHOLDING TAXES (SAWT)');
-		$sheet->getCell('A3')->setValue('FOR THE MONTH OF '.strtoupper($date[0]). ' '.$year);
-
-
-		$sheet->getCell('A6')->setValue('TIN : '.$company->tin);
-		$sheet->getCell('A7')->setValue("PAYEE'S NAME: ".strtoupper($company->companyname));
-
-		$sheet->getCell('A11')->setValue('SEQ');
-		$sheet->getCell('A12')->setValue('NO');
-		$sheet->getCell('A14')->setValue('(1)');
-		$sheet->getCell('A15')->setValue('------------------------------');
-
-		$sheet->getCell('B11')->setValue('TAXPAYER');
-		$sheet->getCell('B12')->setValue('IDENTIFICATION');
-		$sheet->getCell('B13')->setValue('NO');
-		$sheet->getCell('B14')->setValue('(2)');
-		$sheet->getCell('B15')->setValue('------------------------------');
-
-		$sheet->getCell('C11')->setValue('CORPORATION');
-		$sheet->getCell('C12')->setValue('(Registered Name)');
-		$sheet->getCell('C14')->setValue('(3)');
-		$sheet->getCell('C15')->setValue('------------------------------');
-
-		$sheet->getCell('D11')->setValue('INDIVIDUAL');
-		$sheet->getCell('D12')->setValue('(Last Name, First Name, Middle Name)');
-		$sheet->getCell('D14')->setValue('(4)');
-		$sheet->getCell('D15')->setValue('------------------------------');
-
-		$sheet->getCell('E11')->setValue('ATC CODE');
-		$sheet->getCell('E14')->setValue('(5)');
-		$sheet->getCell('E15')->setValue('------------------------------');
-
-		$sheet->getCell('F11')->setValue('NATURE OF PAYMENT');
-		$sheet->getCell('F15')->setValue('------------------------------');
-
-		$sheet->getCell('G11')->setValue('AMOUNT OF');
-		$sheet->getCell('G12')->setValue('INCOME PAYMENT');
-		$sheet->getCell('G14')->setValue('(6)');
-		$sheet->getCell('G15')->setValue('------------------------------');
-
-		$sheet->getCell('H11')->setValue('TAX RATE');
-		$sheet->getCell('H14')->setValue('(7)');
-		$sheet->getCell('H15')->setValue('------------------------------');
-
-		$sheet->getCell('I11')->setValue('AMOUNT OF');
-		$sheet->getCell('I12')->setValue('TAX WITHHELD');
-		$sheet->getCell('I14')->setValue('(8)');
-		$sheet->getCell('I15')->setValue('------------------------------');
-
-		$count = 1;
 		$totalwtaxamount = 0;
-		$cell_row = 16;
+
+		$table .= '"' . implode('","', $header) . '"';
+		$table .= "\n";
+		$table .= '"(1)", "(2)", "(3)", "(4)", "(5)", "", "(6)", "(7)", "(8)"';
+		$table .= "\n";
+		$table .= '"------------------------------", "------------------------------", "------------------------------", "------------------------------", "------------------------------", "------------------------------", "------------------------------", "------------------------------", "------------------------------"';
+		$table .= "\n";
+		$count = 1;
 		if ($pagination) {
-			foreach ($pagination as $row) {
-
+			foreach ($pagination as $key => $row) {
 				$businesstype = $row->businesstype;
-				$contact_person = $row->last_name.', '.$row->first_name;
+				$contact_person = $row->last_name. ' ' .$row->first_name;
 
-				$sheet->getCell('A'.$cell_row)->setValue($count);
-				$sheet->getCell('B'.$cell_row)->setValue($row->tinno);
 				if ($businesstype != 'Individual') {
-					$sheet->getCell('C'.$cell_row)->setValue(strtoupper($row->partnername));
+					$table .= '"'.$count.'", "'.$row->tinno.'", "", "'.strtoupper($contact_person).'", "'.$row->atc_code.'", "'.strtoupper($row->paymenttype).'", "'.number_format($row->taxbase_amount,2).'", "'.number_format($row->tax_rate,2).'", "'.number_format($row->credit,2).'"';
 				}
 				else {
-					$sheet->getCell('D'.$cell_row)->setValue(strtoupper($contact_person));
+					$table .= '"'.$count.'", "'.$row->tinno.'", "'.strtoupper($row->partnername).'", "", "'.$row->atc_code.'", "'.strtoupper($row->paymenttype).'", "'.number_format($row->taxbase_amount,2).'", "'.number_format($row->tax_rate,2).'", "'.number_format($row->credit,2).'"';
 				}
-				$sheet->getCell('E'.$cell_row)->setValue($row->atc_code);
-				$sheet->getCell('F'.$cell_row)->setValue(strtoupper($row->paymenttype));
-				$sheet->getCell('G'.$cell_row)->setValue($row->taxbase_amount);
-				$sheet->getCell('H'.$cell_row)->setValue($row->tax_rate);
-				$sheet->getCell('I'.$cell_row)->setValue($row->credit);
-
+				
+				$table  .= "\n";
+				$totalwtaxamount += $row->credit;
 				$count++;
-				$cell_row++;
-				$totalwtaxamount = $totalwtaxamount + $row->credit;
 			}
 		}
 
 		else {
-			$sheet->mergeCells('A16:I16');
-			$sheet->getCell('A16')->setValue('NO CREDITABLE WITHHOLDING TAX');
-			$sheet->getStyle('A16')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-			$cell_row = $cell_row + 1;
+			$table .= '"NO CREDITABLE WITHHOLDING TAX"';
 		}
+		
+		$table 	.= '"GRAND TOTAL : ","","","", "", "", "", "","'.number_format($totalwtaxamount,2).'"';
+		$table  .= "\n\n";
+		$table  .= '"END OF REPORT"';
 
-		$fortotal_row = $cell_row + 1;
-		$forend_report = $cell_row + 3;
-		$fortotal_amount = $cell_row + 1;
-
-		$sheet->getCell('A'.$fortotal_row)->setValue('Grand Total :');
-		$sheet->getCell('A'.$forend_report)->setValue('END OF REPORT');
-
-		$sheet->getCell('I'.$cell_row)->setValue('------------------');
-		$sheet->getCell('I'.$fortotal_amount)->setValue($totalwtaxamount);
-		// $sheet->getCell('I19')->setValue('==================');
-
-		$sheet->getStyle('G15:I18')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
-
-		foreach ($excel->getAllSheets() as $sheet) {
-			for ($col = 0; $col <= PHPExcel_Cell::columnIndexFromString($sheet->getHighestDataColumn()); $col++) {
-				$sheet->getColumnDimensionByColumn($col)->setAutoSize(true);
-			}
-		}
-
-		$filename.= '.xlsx';
-
-		header('Content-type: application/vnd.ms-excel');
-		header("Content-Disposition: attachment; filename=\"$filename\"");
-		header("Pragma: no-cache");
-		header("Expires: 0");
-
-		flush();
-
-		$writer = PHPExcel_IOFactory::createWriter($excel, 'Excel5');
-
-		$writer->save('php://output');
+		return $table;
 	}
 
 	public function sawt_dat() {
