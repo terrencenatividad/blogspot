f6ac5090a74647a641deca4bb3e82f8112f82ba8
terensnatividad <terrence.natividad@cid-systems.com>
2019-05-16 17:21:34 +0800

    Masonry and layout

--- apanel/view/frontend_header.php
+++ apanel/view/frontend_header.php
@@ -10,32 +10,30 @@
 	<link rel="stylesheet" href="<?= BASE_URL ?>assets/home/css/style.css">
 	<script src="<?= BASE_URL ?>assets/js/jquery.min.js"></script>
 	<script src="<?= BASE_URL ?>assets/js/bootstrap.min.js"></script>
+	<script src="<?= BASE_URL ?>assets/js/masonry.min.js"></script>
 </head>
 <body>
 	<div class="container">
 		<div class="row"></div>
 	</div>
-	<div class="container">
-		<nav class="navbar navbar-inverse">
-			<div class="container-fluid">
-				<div class="col-md-12">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span> 
-						</button>
-						<a class="navbar-brand" href="#"><span class = "fa fa-home"></span></a>
-					</div>
-					<div class="collapse navbar-collapse" id="myNavbar">
-						<ul class="nav navbar-nav">
-							<li class="active"><a href="#">Home</a></li>
-							<li><a href="#">Page 1</a></li>
-							<li><a href="#">Page 2</a></li> 
-							<li><a href="#">Page 3</a></li> 
-						</ul>
-					</div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="col-md-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span> 
+					</button>
+				</div>
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">HOME</a></li>
+						<li><a href="#">BLOGS</a></li>
+						<li><a href="#">ABOUT</a></li>
+						<li><a href="#">CONTACT US</a></li>
+					</ul>
 				</div>
 			</div>
-		</nav>
-	</div>
+		</div>
+	</nav>
